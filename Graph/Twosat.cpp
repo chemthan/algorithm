71a631d744209938d73e713d124789ae905074b9
chemthan <chemthan@gmail.com>
2015-10-04 16:06:21 +0700

    Update

---
+++ Graph/Twosat.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define maxv 20000
+#define pos(v) ((v) << 1)
+#define neg(v) (pos(v) ^ 1)
+namespace twosat {
+	int n;
+	vector<int> forward[maxv], backward[maxv];
+	bool used[maxv];
+	int cnt, order[maxv], comp[maxv];
+	void init(int _n) {
+		n = _n;
+		for (int i = 0; i < n; i++) {
+			forward[i].clear();
+			backward[i].clear();
+		}
+	}
+	void add(int u, int v) {
+		forward[u].push_back(v);
+		backward[v].push_back(u);
+	}
+	void dfs1(int u) {
+		used[u] = true;
+		for (int i = 0; i < forward[u].size(); i++) {
+			int v = forward[u][i];
+			if (!used[v]) dfs1(v);
+		}
+		order[cnt++] = u;
+	}
+	void dfs2(int u, int c) {
+		comp[u] = c;
+		for (int i = 0; i < backward[u].size(); i++) {
+			int v = backward[u][i];
+			if (comp[v] == -1) dfs2(v, c);
+		}
+	}
+	bool solve(vector<int> &res) {
+		cnt = 0;
+		memset(used, 0, sizeof(used));
+		for (int u = 0; u < n; u++) if (!used[u]) dfs1(u);
+		memset(comp, -1, sizeof(comp));
+		int c = 0;
+		for (int i = n - 1; i >= 0; i--) {
+			int u = order[i];
+			if (comp[u] == -1) dfs2(u, c++);
+		}
+		for (int u = 0; u < n; u += 2)
+			if (comp[u] == comp[u ^ 1]) return false;
+		res.clear();
+		for (int u = 0; u < n; u += 2) {
+			int choose = (comp[u] > comp[u ^ 1]) ? u : u ^ 1;
+			if (!(choose & 1)) res.push_back(choose >> 1);
+		}
+		return true;
+	}
+}
+
+int main() {
+    return 0;
+}
---
+++ String/Zfunction.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Zfunction
+//z[i] the length of the longest substr begins at i
+const int maxn = 100010;
+char S[maxn];
+int n;
+int z[maxn];
+void zfunction() {
+	int L = 0, R = 0;
+	z[0] = n;
+	for (int i = 1; i < n; i++) {
+		if (i > R)
+		{
+			L = R = i;
+			while (R < n && S[R] == S[R - L]) R++;
+			z[i] = R - L; R--;
+		}
+		else
+		{
+			int k = i - L;
+			if (z[k] < R - i + 1) z[i] = z[k];
+			else
+			{
+				L = i;
+				while (R < n && S[R] == S[R - L]) R++;
+				z[i] = R - L; R--;
+			}
+		}
+	}
+}
+
+int main() {
+    strcpy(S, "stringsrandom");
+    n = strlen(S);
+    zfunction();
+    for (int i = 0; i < n; i++) printf("%d ", z[i]);
+    return 0;
+}
