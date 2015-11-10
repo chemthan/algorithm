8394e073edf5eecec75b37da0ce67433b08fe92f
Trung Nguyen <chemthan@users.noreply.github.com>
2015-11-10 10:52:06 +0700

    Update Lyndon.cpp

--- String/Lyndon.cpp
+++ String/Lyndon.cpp
@@ -3,14 +3,13 @@ using namespace std;
 
 //O(n)
 void lyndon(string s) {
-	int n = (int) s.length();
-	int i = 0;
+	int n = s.size(), i = 0;
 	while (i < n) {
 		int j = i + 1, k = i;
 		while (j < n && s[k] <= s[j]) {
 			if (s[k] < s[j]) k = i;
-			else ++k;
-			++j;
+			else k++;
+			j++;
 		}
 		while (i <= k) {
 			cout<<s.substr(i, j - k)<<" ";
