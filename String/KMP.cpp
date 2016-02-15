df93db667e9759fe1195df7b27a700942230a446
chemthan <chemthan@gmail.com>
2016-02-15 20:53:29 +0700

    Update

--- String/KMP.cpp
+++ String/KMP.cpp
@@ -1,37 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//O(nlogn)
+//O(N + M)
 int const MAXN = 100010;
-int b[MAXN];
-char T[MAXN];
+int nP;
 char P[MAXN];
-int nT, nP;
-void preprocess() {
-	int i = 0, j = -1; b[0] = -1;
-	while (i < nP) {
-		while (j >= 0 && P[i] != P[j]) j = b[j];
-		i++; j++;
-		b[i] = j;
+int r[MAXN];
+void KMP() {
+	r[0] = -1;
+	int cur = -1;
+	for (int i = 0; i < nP; i++) {
+		while (cur >= 0 && P[i] != P[cur]) cur = r[cur];
+		r[i + 1] = ++cur;
 	}
 }
-void search() {
-	preprocess();
-	int i = 0, j = 0;
-	while (i < nT) {
-		while (j >= 0 && T[i] != P[j]) j = b[j];
-		i++; j++;
-		if (j == nP) {
-			printf("%d ", i - j);
-			j = b[j];
+void search(char* T) {
+	int nT = strlen(T), cur = 0;
+	for (int i = 0; i < nT; i++) {
+		while (cur >= 0 && T[i] != P[cur]) cur = r[cur];
+		if (++cur == nP) {
+			cout<<i - cur + 1<<" ";
 		}
 	}
 }
 
 int main() {
-	strcpy(T, "123123123");
-	strcpy(P, "123");
-	nT = strlen(T); nP = strlen(P);
-	search();
+	strcpy(P, "123123");
+	nP = strlen(P); KMP();
+	search((char*) "412312312312312312341231231231234"); //Expect 1 4 7 10 13 20 23 26
 	return 0;
 }
