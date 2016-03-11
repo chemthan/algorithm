5a8da4951ceec0699320852016b2aa17034a7f77
chemthan <chemthan@gmail.com>
2016-03-12 03:03:39 +0700

    Update

--- Math/Rabin.cpp
+++ Math/Rabin.cpp
@@ -1,35 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long mul(long long a, long long b, long long mod) {
-	return a * b % mod;
+#define T long long
+T mulmod64(T a, T b, T p) {
+	a %= p; b %= p;
+	T r = 0;
+	int block = 1;
+	T base = 1LL << block;
+	for (; b; b >>= block) {
+		r = (r + a * (b & (base - 1))) % p;
+		a = a * base % p;
+	}
+	return r;
 }
-long long pow(long long a, long long k, long long mod) {
-	if (k == 0) return 1;
-	if (k & 1) return a * pow(a, k - 1, mod) % mod;
-	long long t = pow(a, k >> 1, mod);
-	return mul(t, t, mod);
+T powmod64(T n, T k, T p) {
+	if (!n) return 0;
+	T r = 1;
+	for (; k; k >>= 1) {
+		if (k & 1) r = mulmod64(r, n, p);
+		n = mulmod64(n, n, p);
+	}
+	return r;
 }
-bool rabin(long long n) {
-	if (n == 2) return true;
-	if (n < 2 || !(n & 1)) return false;
-	const long long p[3] = {3, 5, 7};
-	long long a, d = n - 1, mx = 3;
+int rabin(T n) {
+	if (n == 2) return 1;
+	if (n < 2 || !(n & 1)) return 0;
+	const T p[3] = {3, 5, 7};
+	T a, d = n - 1, mx = 3;
 	int i, r, s = 0;
 	while (!(d & 1)) {++s; d >>= 1;}
 	for (i = 0; i < mx; i++) {
-		if (n == p[i]) return true;
-		if (!(n % p[i])) return false;
-		a = pow(p[i], d, n);
+		if (n == p[i]) return 1;
+		if (!(n % p[i])) return 0;
+		a = powmod64(p[i], d, n);
 		if (a != 1) {
-			for (r = 0; r < s && a != n - 1; r++) a = mul(a, a, n);
-			if (r == s) return false;
+			for (r = 0; r < s && a != n - 1; r++) a = mulmod64(a, a, n);
+			if (r == s) return 0;
 		}
 	}
-	return true;
+	return 1;
 }
 
 int main() {
-	printf("%d", rabin(1000000007));
+	cout<<rabin(1000000007)<<"\n";
+	cout<<rabin(100000007)<<"\n";
+	cout<<rabin(10000007)<<"\n";
+	cout<<rabin(1000007)<<"\n";
+	cout<<rabin(100007)<<"\n";
+	cout<<rabin(10007)<<"\n";
+	cout<<rabin(1007)<<"\n";
+	cout<<rabin(107)<<"\n";
+	cout<<rabin(17)<<"\n";
 	return 0;
 }
