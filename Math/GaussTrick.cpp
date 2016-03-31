fe6a5cc9d98eb22c528e97130ad5f3b436e87072
chemthan <chemthan@gmail.com>
2016-03-31 22:47:48 +0700

    Update

---
+++ Math/GaussTrick.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void add(int x, vector<int>& v) {
+	for (int i = 0; i < v.size(); i++) {
+		if (v[i] & -v[i] & x) x ^= v[i];
+	}
+	if (!x) return;
+	for (int i = 0; i < v.size(); i++) {
+		if (x & - x & v[i]) v[i] ^= x;
+	}
+	v.push_back(x);
+}
+
+int main() {
+	vector<int> v;
+	add(1, v);
+	add(2, v);
+	add(3, v);
+	add(4, v);
+	cout<<v.size()<<"\n";
+	return 0;
+}
