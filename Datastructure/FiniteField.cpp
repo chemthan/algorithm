b6c0fd475feaa2c1372abd5dc31d84cbfe6e4711
chemthan <chemthan@gmail.com>
2017-02-03 17:08:54 +0700

    Update

--- Datastructure/FiniteField.cpp
+++ Datastructure/FiniteField.cpp
@@ -1,102 +1,121 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template<class T, T p, class mul, class dvd> struct FiniteField {
+template<class T, class mul, class dvd, class mod> struct FiniteField {
 	T x;
 	template<class G> T normalize(G x) {
+		T p = mod().get();
 		if (x >= 0 && x < p) return x;
 		x %= p;
 		if (x < 0) x += p;
 		else if (x >= p) x -= p;
 		return x;
 	}
-	FiniteField() {
-		x = 0;
-	}
+	FiniteField() : x(0) {}
 	template<class G> FiniteField(const G& y) {
 		x = normalize(y);
 	}
-	FiniteField(const FiniteField& rhs) {
-		x = rhs.x;
-	}
-	template<class G> FiniteField operator () (const G& y) {
-		x = normalize(y);
-		return *this;
+	FiniteField(const FiniteField& rhs) : x(rhs.x) {
 	}
 	template<class G> FiniteField operator = (const G& y) {
 		x = normalize(y);
 		return *this;
 	}
+	int operator == (const FiniteField& rhs) const {
+		return x == rhs.x;
+	}
+	int operator != (const FiniteField& rhs) const {
+		return x != rhs.x;
+	}
+	int operator < (const FiniteField& rhs) const {
+		return x < rhs.x;
+	}
 	FiniteField operator ++(int) {
+		T p = mod().get();
 		FiniteField res = *this;
 		if ((++x) >= p) x -= p;
 		return res;
 	}
 	FiniteField operator ++() {
+		T p = mod().get();
 		if ((++x) >= p) x -= p;
 		return *this;
 	}
 	FiniteField operator --(int) {
+		T p = mod().get();
 		FiniteField res = *this;
 		if ((--x) < 0) x += p;
 		return res;
 	}
 	FiniteField operator --() {
+		T p = mod().get();
 		if ((--x) < 0) x += p;
 		return *this;
 	}
 	template<class G> FiniteField operator += (const G& y) {
+		T p = mod().get();
 		if ((x += normalize(y)) >= p) x -= p;
 		return *this;
 	}
 	template<class G> FiniteField operator -= (const G& y) {
+		T p = mod().get();
 		if ((x -= normalize(y)) < 0) x += p;
 		return *this;
 	}
 	FiniteField operator += (const FiniteField& rhs) {
+		T p = mod().get();
 		if ((x += rhs.x) >= p) x -= p;
 		return *this;
 	}
 	FiniteField operator -= (const FiniteField& rhs) {
+		T p = mod().get();
 		if ((x -= rhs.x) < 0) x += p;
 		return *this;
 	}
 	FiniteField operator + (const FiniteField& rhs) const {
+		T p = mod().get();
 		FiniteField res;
 		res.x = x + rhs.x;
 		if (res.x >= p) res.x -= p;
 		return res;
 	}
 	FiniteField operator - (const FiniteField& rhs) const {
+		T p = mod().get();
 		FiniteField res;
 		res.x = x - rhs.x;
 		if (res.x < 0) res.x += p;
 		return res;
 	}
 	template<class G> FiniteField operator *= (const G& y) {
+		T p = mod().get();
 		x = mul()(x, normalize(y), p);
 		return *this;
 	}
 	template<class G> FiniteField operator /= (const G& y) {
+		T p = mod().get();
 		x = dvd()(x, normalize(y), p);
 		return *this;
 	}
 	template<class G> FiniteField operator * (const G& y) const {
+		T p = mod().get();
 		FiniteField res;
 		res.x = mul()(x, res.normalize(y), p);
 		return res;
 	}
 	template<class G> FiniteField operator / (const G& y) const {
+		T p = mod().get();
 		FiniteField res;
 		res.x = dvd()(x, res.normalize(y), p);
 		return res;
 	}
 	FiniteField operator * (const FiniteField& rhs) const {
+		T p = mod().get();
 		FiniteField res;
 		res.x = mul()(x, rhs.x, p);
 		return res;
 	}
 	FiniteField operator / (const FiniteField& rhs) const {
+		T p = mod().get();
 		FiniteField res;
 		res.x = dvd()(x, rhs.x, p);
 		return res;
@@ -112,6 +131,7 @@ template<class T, T p, class mul, class dvd> struct FiniteField {
 		return res / rhs;
 	}
 	template<class G> FiniteField operator ^= (G k) {
+		T p = mod().get();
 		if (k == 0) {
 			x = 1;
 			return *this;
@@ -125,6 +145,7 @@ template<class T, T p, class mul, class dvd> struct FiniteField {
 		return *this;
 	}
 	template<class G> FiniteField operator ^ (G k) const {
+		T p = mod().get();
 		if (k == 0) return 1;
 		T r = x, t = x; k--;
 		while (k > 0) {
@@ -134,11 +155,15 @@ template<class T, T p, class mul, class dvd> struct FiniteField {
 		}
 		return r;
 	}
+	friend istream& operator >> (istream& is, FiniteField& rhs) {
+		is >> rhs.x;
+		rhs.x = rhs.normalize(rhs.x);
+		return is;
+	}
 	friend ostream& operator << (ostream& os, const FiniteField& rhs) {
 		os << rhs.x;
 		return os;
 	}
-	
 };
 template<class T> class mul {
 	public:
@@ -159,10 +184,17 @@ template<class T> class dvd {
 		return make_pair(r.second, r.first - a / b * r.second);
 	}
 };
-typedef FiniteField<int, (int) 1e9 + 7, mul<int>, dvd<int> > FF;
+template<class T> class mod {
+	public:
+	T get() {
+		return (T) 1e9 + 7;
+	}
+};
+typedef FiniteField<int, mul<int>, dvd<int>, mod<int> > FF;
 
 int main() {
 	FF a = 7;
+	cin >> a;
 	cout << a / 11 * 11 << "\n";
 	cout << a + a << "\n";
 	cout << a++ << "\n";
