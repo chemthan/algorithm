cb2be91c5af807d0c65cc7506e37458411d6b3f1
chemthan <chemthan@gmail.com>
2017-02-27 00:47:10 +0700

    Update

--- Datastructure/CascadingSegment.cpp
+++ Datastructure/CascadingSegment.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -59,65 +58,4 @@ int main() {
     cout << cs.query(1, 3, 5, 0, n - 1, 10) << "\n";
     return 0;
 }
-=======
-#include <bits/stdc++.h>
-using namespace std;
 
-/*
- * Complexity: O(NlogN)
- */
-template<class T> struct CascadingSegment {
-    struct Info {
-        T key;
-        int lnxt, rnxt;
-        Info() : key(0), lnxt(0), rnxt(0) {}
-        Info(T key) : key(key), lnxt(0), rnxt(0) {}
-        int operator < (const Info& rhs) const {
-            return key < rhs.key;
-        }
-    };
-    
-    static const int maxn = 1e5 + 5;
-    vector<Info> st[maxn << 2];
-    
-    //Updating val in increasing order
-    void upd(int p, int i, int L, int R, T val) {
-        if (i < L || i > R) return;
-        int lptr = st[p].size() ? st[p].back().lnxt : 0;
-        int rptr = st[p].size() ? st[p].back().rnxt : 0;
-        st[p].push_back(Info(val));
-        if (L < R) {
-            upd(p << 1, i, L, L + R >> 1, val);
-            upd(p << 1 | 1, i, (L + R >> 1) + 1, R, val);
-            while (lptr < st[p << 1].size() && val >= st[p << 1][lptr].key) lptr++;
-            while (rptr < st[p << 1 | 1].size() && val >= st[p << 1 | 1][rptr].key) rptr++;
-        }
-        st[p].back().lnxt  = lptr, st[p].back().rnxt = rptr;
-    }
-    
-    //Calculating how many numbers in range [l, r] less than or equal val
-    int query(int p, int l, int r, int L, int R, T val) {
-        if (l > R || r < L) return 0;
-        if (p == 1) {
-            int k = upper_bound(st[p].begin(), st[p].end(), Info(val)) - st[p].begin() - 1;
-            if (!~k) return 0;
-            if (l <= L && r >= R) return k + 1;
-            return query(p << 1, l, r, L, L + R >> 1, st[p][k].lnxt - 1) + query(p << 1 | 1, l, r, (L + R >> 1) + 1, R, st[p][k].rnxt - 1);
-        }
-        if (!~val) return 0;
-        if (l <= L && r >= R) return val + 1;
-        return query(p << 1, l, r, L, L + R >> 1, st[p][val].lnxt - 1) + query(p << 1 | 1, l, r, (L + R >> 1) + 1, R, st[p][val].rnxt - 1);
-    }
-};
-
-CascadingSegment<int> cs;
-
-int main() {
-    int n = 100;
-    cs.upd(1, 3, 0, n - 1, 5);
-    cout << cs.query(1, 0, 3, 0, n - 1, 5) << "\n";
-    cs.upd(1, 5, 0, n - 1, 10);
-    cout << cs.query(1, 3, 5, 0, n - 1, 10) << "\n";
-    return 0;
-}
->>>>>>> f173cdf19824eaeb2fcfae7078c92d5e11319780
--- Datastructure/RegionTree.cpp
+++ Datastructure/RegionTree.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -74,80 +73,4 @@ struct RegionTree {
 int main() {
     return 0;
 }
-=======
-#include <bits/stdc++.h>
-using namespace std;
 
-int ix;
-int compare(int x, int y) {
-    if (x > ix && y < ix) return 0;
-    if (x < ix && y > ix) return 1;
-    return x > y;
-}
-struct RegionTree {
-    static const int maxn = 1e5 + 5;
-    int n;
-    vector<int> adj[maxn]; //diagonals and edges
-    int cnt; //number of regions
-    map<pair<int, int>, int> hs; //hs[a, b] index of region below diagonal (a, b)
-    vector<pair<int, int> > g[maxn]; //edges of a region
-    vector<int> tree[maxn]; //region tree
-    
-    void init(int n) {
-        this->n = n;
-        for (int i = 0; i < n; i++) {
-            adj[i].clear();
-            tree[i].clear();
-        }
-        cnt = 0;
-        hs.clear();
-        for (int i = 0; i < n; i++) {
-            g[i].clear();
-        }
-    }
-    void add(int u, int v) {
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-    void build() {
-        for (int i = 0; i < n; i++) {
-            int j = (i + 1) % n;
-            adj[i].push_back(j);
-            adj[j].push_back(i);
-        }
-        for (int i = 0; i < n; i++) {
-            ix = i;
-            sort(adj[i].begin(), adj[i].end(), compare);
-        }
-        for (int u = n - 1; u >= 0; u--) {
-            for (int i = 0; i < (int) adj[u].size() - 1; i++) {
-                int v = adj[u][i + 1];
-                int w = adj[u][i];
-                if (!hs.count(make_pair(u, v))) {
-                    hs[make_pair(u, v)] = cnt;
-                    g[cnt].push_back(make_pair(u, v));
-                    cnt++;
-                }
-                int ix = hs[make_pair(u, v)];
-                if (!hs.count(make_pair(w, u))) {
-                    g[ix].push_back(make_pair(w, u));
-                }
-                hs[make_pair(w, u)] = ix;
-            }
-        }
-        for (int i = 0; i < cnt; i++) {
-            for (int j = 0; j < (int) g[i].size(); j++) {
-                pair<int, int> p = g[i][j];
-                swap(p.first, p.second);
-                if (hs.count(p)) {
-                    tree[i].push_back(hs[p]);
-                }
-            }
-        }
-    }
-} rt;
-
-int main() {
-    return 0;
-}
->>>>>>> f173cdf19824eaeb2fcfae7078c92d5e11319780
