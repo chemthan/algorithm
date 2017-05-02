638a6e89c07c7d830fb30827d529a63b870a1973
Trung Nguyen <chemthan@users.noreply.github.com>
2017-05-02 11:38:19 +0700

    Update

--- String/MinMove.cpp
+++ String/MinMove.cpp
@@ -7,26 +7,26 @@ using namespace std;
 */
 int minmove(char* s) {
     int n = strlen(s);
-	int x, y, i, j, u, v; //x is the smallest string before string y
-	for (x = 0, y = 1; y < n; y++) {
-		i = u = x;
-		j = v = y;
-		while (s[i] == s[j]) {
-			u++, v++;
-			if (++i == n) i = 0;
-			if (++j == n) j = 0;
-			if (i == x) break; //all strings are equal
-		}
-		if (s[i] <= s[j]) y = v;
-		else {
-			x = y;
-			if (u > y) y = u;
-		}
-	}
-	return x;
+    int x, y, i, j, u, v; //x is the smallest string before string y
+    for (x = 0, y = 1; y < n; y++) {
+        i = u = x;
+        j = v = y;
+        while (s[i] == s[j]) {
+            u++, v++;
+            if (++i == n) i = 0;
+            if (++j == n) j = 0;
+            if (i == x) break; //all strings are equal
+        }
+        if (s[i] <= s[j]) y = v;
+        else {
+            x = y;
+            if (u > y) y = u;
+        }
+    }
+    return x;
 }
 
 int main() {
-	cout << minmove((char*) "stringrandom") << "\n";
-	return 0;
+    cout << minmove((char*) "stringrandom") << "\n";
+    return 0;
 }
