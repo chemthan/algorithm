760893e1f8c70bab2337cd1f96f08e08b4e40fb3
Trung Nguyen <chemthan@users.noreply.github.com>
2017-05-02 23:06:42 +0700

    Update CascadingSegment.cpp

--- Datastructure/CascadingSegment.cpp
+++ Datastructure/CascadingSegment.cpp
@@ -14,7 +14,6 @@ template<class T> struct CascadingSegment {
             return key < rhs.key;
         }
     };
-    
     static const int maxn = 1e5 + 5;
     vector<Info> st[maxn << 2];
     
@@ -32,7 +31,6 @@ template<class T> struct CascadingSegment {
         }
         st[p].back().lnxt  = lptr, st[p].back().rnxt = rptr;
     }
-    
     //Calculating how many numbers in range [l, r] less than or equal val
     int query(int p, int l, int r, int L, int R, T val) {
         if (l > R || r < L) return 0;
@@ -47,7 +45,6 @@ template<class T> struct CascadingSegment {
         return query(p << 1, l, r, L, L + R >> 1, st[p][val].lnxt - 1) + query(p << 1 | 1, l, r, (L + R >> 1) + 1, R, st[p][val].rnxt - 1);
     }
 };
-
 CascadingSegment<int> cs;
 
 int main() {
