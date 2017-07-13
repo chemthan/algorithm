7c1563116e25d3a6c60e48ee2e2d60641172a1ae
Trung Nguyen <chemthan@users.noreply.github.com>
2017-07-13 21:50:07 +0700

    Update

--- DP Optimization/DivideFFT.cpp
+++ DP Optimization/DivideFFT.cpp
@@ -2,77 +2,80 @@
 using namespace std;
 
 /*
-* Complexity: Nlog(N)^2
+* Complexity: N(logN)^2
 * Solve dp formula: a[i] = a[i - 1] * b[0] + a[i - 2] * b[1] + ... + a[0] * b[i - 1]
 * Problems:
 * 1. http://codeforces.com/contest/553/problem/E
 * 2. http://codeforces.com/gym/100959/status/H
 * 3. https://www.bnuoj.com/v3/problem_show.php?pid=52248
 */
-typedef long double T;
-const int MAXN = 1 << 20;
-struct cplex {
-    T r, i;
-    cplex() : r(0), i(0) {}
-    cplex(T n) : r(n), i(0) {}
-    cplex(T r, T i) : r(r), i(i) {}
-    T real() {return r;}
-    cplex operator + (cplex b) {return cplex (r + b.r, i + b.i);}
-    void operator += (cplex b) {r += b.r, i += b.i;}
-    cplex operator - (cplex b) {return cplex(r - b.r, i - b.i);}
-    cplex operator * (cplex b) {return cplex(r * b.r - i * b.i, r * b.i + i * b.r);}
-    cplex operator *= (cplex b) {T r2 = r * b.r - i * b.i, i2 = r * b.i + i * b.r; r = r2, i = i2;}
-    void operator /= (T n) {r /= n, i /= n;}
+typedef long double db;
+struct cp {
+    db x, y;
+    cp(db x = 0, db y = 0) : x(x), y(y) {}
+    cp operator + (const cp& rhs) {return cp(x + rhs.x, y + rhs.y);}
+    cp operator - (const cp& rhs) {return cp(x - rhs.x, y - rhs.y);}
+    cp operator * (const cp& rhs) {return cp(x * rhs.x - y * rhs.y, x * rhs.y + y * rhs.x);}
+    cp operator !() {return cp(x, -y);}
 };
-void fft(cplex a[], int n, int invert) {
-    static const T PI = 2 * acos((T) 0);
-    for (int i = 1, j = 0; i < n; i++) {
-        for (int s = n; j ^= s >>= 1, ~j & s;);
-        if (i < j) swap(a[i], a[j]);
-    }
-    for (int m = 1; m < n; m <<= 1) {
-        T p = PI / m * (invert ? -1 : 1);
-        cplex w = cplex(cos(p), sin(p));
-        for (int i = 0; i < n; i += m << 1) {
-            cplex unit = 1;
-            for (int j = 0; j < m; j++) {
-                cplex& x = a[i + j + m], &y = a[i + j], t = unit * x;
-                x = y - t;
-                y = y + t;
-                unit *= w;
+void fft(cp a[], int n, int invert) {
+    static const db PI = acos((db) -1);
+    cp w = cp(cos(2 * PI / n), sin(2 * PI / n));
+    if (invert) w = !w;
+    for (int k = n / 2; k >= 1; k >>= 1, w = w * w) {
+        cp t = cp(1, 0);
+        for (int i = 0; i < k; i++, t = t * w) {
+            for (int j = i; j + k < n; j += k + k) {
+                cp x = a[j];
+                cp y = a[j + k];
+                a[j] = x + y;
+                a[j + k] = (x - y) * t;
             }
         }
     }
-    if (invert) for (int i = 0; i < n; i++) a[i] /= n;
+    int k = __lg(n);
+    for (int msk = 0; msk < n; msk++) {
+        int nmsk = 0;
+        for (int i = 0; i < k; i++) {
+            if ((msk >> i) & 1) {
+                nmsk |= 1 << k - i - 1;
+            }
+        }
+        if (msk < nmsk) {
+            swap(a[msk], a[nmsk]);
+        }
+    }
+    if (invert) {
+        for (int i = 0; i < n; i++) {
+            a[i].x /= n;
+            a[i].y /= n;
+        }
+    }
 }
-void modularmultiply(int a[], int b[], int c[], int na, int nb, int mod = (int) 1e9 + 7) {
-    static const int K = 2;
-    static cplex ap[K][MAXN << 1], bp[K][MAXN << 1], cc[MAXN << 1];
-    int n = 1; while (n < na + nb - 1) n <<= 1;
-    int base = (int) pow(mod, 1.0 / K) + 1;
+void multiply(int a[], int b[], int c[], int na, int nb, int mod = (int) 1e9 + 7) {
+    static cp fa[1 << 20], fb[1 << 20];
+    static cp fc[1 << 20], fd[1 << 20];
+    int n = na + nb - 1;
+    while (n != (n & -n)) n += n & -n;
+    for (int i = 0; i < n; i++) fa[i] = fb[i] = cp(0, 0);
+    for (int i = 0; i < na; i++) fa[i] = cp(a[i] >> 15, a[i] & (1 << 15) - 1);
+    for (int i = 0; i < nb; i++) fb[i] = cp(b[i] >> 15, b[i] & (1 << 15) - 1);
+    fft(fa, n, 0), fft(fb, n, 0);
     for (int i = 0; i < n; i++) {
-        int ta = i < na ? a[i] : 0, tb = i < nb ? b[i] : 0;
-        for (int j = 0; j < K; j++) {
-            ap[j][i] = ta % base;
-            ta /= base;
-            bp[j][i] = tb % base;
-            tb /= base;
-        }
+        int j = (n - i) % n;
+        cp x = fa[i] + !fa[j];
+        cp y = fb[i] + !fb[j];
+        cp z = !fa[j] - fa[i];
+        cp t = !fb[j] - fb[i];
+        fc[i] = (x * t + y * z) * cp(0, 0.25);
+        fd[i] = x * y * cp(0, 0.25) + z * t * cp(-0.25, 0);
     }
-    for (int i = 0; i < K; i++) {
-        fft(ap[i], n, 0);
-        fft(bp[i], n, 0);
-    }
-    for (int i = 0; i < n; i++) c[i] = 0;
-    for (int i = 0; i < K; i++) for (int j = 0; j < K; j++) {
-        for (int k = 0; k < n; k++) cc[k] = ap[i][k] * bp[j][k];
-        fft(cc, n, 1);
-        for (int k = 0; k < n; k++) {
-            long long z = (long long) (cc[k].real() + 0.5);
-            z = (z % mod + mod) % mod;
-            for (int l = 0; l < i + j; l++) z = z * base % mod;
-            c[k] += z; if (c[k] >= mod) c[k] -= mod;
-        }
+    fft(fc, n, 1), fft(fd, n, 1);
+    for (int i = 0; i < n; i++) {
+        long long u = ((long long) floor(fc[i].x + 0.5)) % mod;
+        long long v = ((long long) floor(fd[i].x + 0.5)) % mod;
+        long long w = ((long long) floor(fd[i].y + 0.5)) % mod;
+        c[i] = ((u << 15) + v + (w << 30)) % mod;
     }
 }
 
@@ -93,10 +96,8 @@ int main() {
     }
     for (int i = 0; i < n; i++) {
         //a[i] has been computed
-        a[i + 1] = (a[i + 1] + (long long) a[i] * b[0]) % mod;
-        a[i + 2] = (a[i + 2] + (long long) a[i] * b[1]) % mod;
-        for (int k = 2; i && i % k == 0; k <<= 1) {
-            modularmultiply(a + i - k, b + k, c, k, k, mod);
+        for (int k = 1; (i + 1) % k == 0; k <<= 1) {
+            multiply(a + i - k + 1, b + k - 1, c, k, k, mod);
             for (int j = i + 1; j < i + 2 * k; j++) {
                 a[j] = (a[j] + c[j - i - 1]) % mod;
             }
--- Math/FFT.cpp
+++ Math/FFT.cpp
@@ -1,108 +1,110 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long double T;
-const int MAXN = 1 << 20;
-//typedef complex<T> cplex;
-struct cplex {
-    T r, i;
-    cplex() : r(0), i(0) {}
-    cplex(T n) : r(n), i(0) {}
-    cplex(T r, T i) : r(r), i(i) {}
-    T real() {return r;}
-    cplex operator + (cplex b) {return cplex (r + b.r, i + b.i);}
-    cplex operator += (cplex b) {r += b.r, i += b.i; return *this;}
-    cplex operator - (cplex b) {return cplex(r - b.r, i - b.i);}
-    cplex operator * (cplex b) {return cplex(r * b.r - i * b.i, r * b.i + i * b.r);}
-    cplex operator *= (cplex b) {T r2 = r * b.r - i * b.i, i2 = r * b.i + i * b.r; r = r2, i = i2; return *this;}
-    cplex operator /= (T n) {r /= n, i /= n; return *this;}
+typedef long double db;
+struct cp {
+    db x, y;
+    cp(db x = 0, db y = 0) : x(x), y(y) {}
+    cp operator + (const cp& rhs) {return cp(x + rhs.x, y + rhs.y);}
+    cp operator - (const cp& rhs) {return cp(x - rhs.x, y - rhs.y);}
+    cp operator * (const cp& rhs) {return cp(x * rhs.x - y * rhs.y, x * rhs.y + y * rhs.x);}
+    cp operator !() {return cp(x, -y);}
 };
-void fft(cplex a[], int n, int invert) {
-    static T PI = 2 * acos((T) 0);
-    for (int i = 1, j = 0; i < n; i++) {
-        for (int s = n; j ^= s >>= 1, ~j & s;);
-        if (i < j) swap(a[i], a[j]);
-    }
-    for (int m = 1; m < n; m <<= 1) {
-        T p = PI / m * (invert ? -1 : 1);
-        cplex w = cplex(cos(p), sin(p));
-        for (int i = 0; i < n; i += m << 1) {
-            cplex unit = 1;
-            for (int j = 0; j < m; j++) {
-                cplex& x = a[i + j + m], &y = a[i + j], t = unit * x;
-                x = y - t;
-                y = y + t;
-                unit *= w;
+void fft(cp a[], int n, int invert) {
+    static const db PI = acos((db) -1);
+    cp w = cp(cos(2 * PI / n), sin(2 * PI / n));
+    if (invert) w = !w;
+    for (int k = n / 2; k >= 1; k >>= 1, w = w * w) {
+        cp t = cp(1, 0);
+        for (int i = 0; i < k; i++, t = t * w) {
+            for (int j = i; j + k < n; j += k + k) {
+                cp x = a[j];
+                cp y = a[j + k];
+                a[j] = x + y;
+                a[j + k] = (x - y) * t;
             }
         }
     }
-    if (invert) for (int i = 0; i < n; i++) a[i] /= n;
+    int k = __lg(n);
+    for (int msk = 0; msk < n; msk++) {
+        int nmsk = 0;
+        for (int i = 0; i < k; i++) {
+            if ((msk >> i) & 1) {
+                nmsk |= 1 << k - i - 1;
+            }
+        }
+        if (msk < nmsk) {
+            swap(a[msk], a[nmsk]);
+        }
+    }
+    if (invert) {
+        for (int i = 0; i < n; i++) {
+            a[i].x /= n;
+            a[i].y /= n;
+        }
+    }
 }
 void multiply(int a[], int b[], long long c[], int na, int nb) {
-    static cplex fa[MAXN << 1], fb[MAXN << 1];
-    int n = 1; while (n < na + nb - 1) n <<= 1;
-    for (int i = 0; i < n; i++) fa[i] = fb[i] = cplex(0);
-    for (int i = 0; i < na; i++) fa[i] = cplex(a[i]);
-    for (int i = 0; i < nb; i++) fb[i] = cplex(b[i]);
-    fft(fa, n, 0); fft(fb, n, 0);
-    for (int i = 0; i < n; i++) fa[i] *= fb[i];
+    static cp fa[1 << 20];
+    static cp fb[1 << 20];
+    int n = na + nb;
+    while (n != (n & -n)) n += n & -n;
+    for (int i = 0; i < n; i++) fa[i] = fb[i] = cp(0, 0);
+    for (int i = 0; i < na; i++) fa[i] = cp(a[i], 0);
+    for (int i = 0; i < nb; i++) fb[i] = cp(b[i], 0);
+    fft(fa, n, 0), fft(fb, n, 0);
+    for (int i = 0; i < n; i++) fa[i] = fa[i] * fb[i];
     fft(fa, n, 1);
-    for (int i = 0; i < n; i++) c[i] = (long long) (fa[i].real() + 0.5);
+    for (int i = 0; i < n; i++) c[i] = (long long) (fa[i].x + 0.5);
 }
-void modularmultiply(int a[], int b[], int c[], int na, int nb, int mod) {
-    static const int K = 2;
-    static cplex ap[K][MAXN << 1], bp[K][MAXN << 1], cc[MAXN << 1];
-    int n = 1; while (n < na + nb - 1) n <<= 1;
-    int base = (int) pow(mod, 1.0 / K) + 1;
+void multiply(int a[], int b[], int c[], int na, int nb, int mod = (int) 1e9 + 7) {
+    static cp fa[1 << 20], fb[1 << 20];
+    static cp fc[1 << 20], fd[1 << 20];
+    int n = na + nb - 1;
+    while (n != (n & -n)) n += n & -n;
+    for (int i = 0; i < n; i++) fa[i] = fb[i] = cp(0, 0);
+    for (int i = 0; i < na; i++) fa[i] = cp(a[i] >> 15, a[i] & (1 << 15) - 1);
+    for (int i = 0; i < nb; i++) fb[i] = cp(b[i] >> 15, b[i] & (1 << 15) - 1);
+    fft(fa, n, 0), fft(fb, n, 0);
     for (int i = 0; i < n; i++) {
-        int ta = i < na ? a[i] : 0, tb = i < nb ? b[i] : 0;
-        for (int j = 0; j < K; j++) {
-            ap[j][i] = ta % base;
-            ta /= base;
-            bp[j][i] = tb % base;
-            tb /= base;
-        }
+        int j = (n - i) % n;
+        cp x = fa[i] + !fa[j];
+        cp y = fb[i] + !fb[j];
+        cp z = !fa[j] - fa[i];
+        cp t = !fb[j] - fb[i];
+        fc[i] = (x * t + y * z) * cp(0, 0.25);
+        fd[i] = x * y * cp(0, 0.25) + z * t * cp(-0.25, 0);
     }
-    for (int i = 0; i < K; i++) {
-        fft(ap[i], n, 0);
-        fft(bp[i], n, 0);
-    }
-    for (int i = 0; i < n; i++) c[i] = 0;
-    for (int i = 0; i < K; i++) for (int j = 0; j < K; j++) {
-        for (int k = 0; k < n; k++) cc[k] = ap[i][k] * bp[j][k];
-        fft(cc, n, 1);
-        for (int k = 0; k < n; k++) {
-            long long z = (long long) (cc[k].real() + 0.5);
-            z = (z % mod + mod) % mod;
-            for (int l = 0; l < i + j; l++) z = z * base % mod;
-            c[k] += z; if (c[k] >= mod) c[k] -= mod;
-        }
+    fft(fc, n, 1), fft(fd, n, 1);
+    for (int i = 0; i < n; i++) {
+        long long u = ((long long) floor(fc[i].x + 0.5)) % mod;
+        long long v = ((long long) floor(fd[i].x + 0.5)) % mod;
+        long long w = ((long long) floor(fd[i].y + 0.5)) % mod;
+        c[i] = ((u << 15) + v + (w << 30)) % mod;
     }
 }
 
-const int MOD = (int) 1e9 + 7;
-int a[MAXN];
-int b[MAXN];
-int c[MAXN << 1];
-long long d[MAXN << 1];
+const int maxn = 1 << 20;
+const int mod = (int) 1e9 + 7;
+int a[maxn];
+int b[maxn];
+int c[maxn];
+long long d[maxn];
 
 int main() {
     srand(time(NULL));
     int na = 10000, nb = 10000;
     for (int i = 0; i < na; i++) a[i] = rand() * rand() % 1000000000;
     for (int i = 0; i < nb; i++) b[i] = rand() * rand() % 1000000000;
-    modularmultiply(a, b, c, na, nb, MOD);
+    multiply(a, b, c, na, nb, mod);
     for (int i = 0; i < na; i++) {
         for (int j = 0; j < nb; j++) {
             d[i + j] += (long long) a[i] * b[j];
-            d[i + j] %= MOD;
+            d[i + j] %= mod;
         }
     }
     for (int i = 0; i < na + nb - 1; i++) {
-        if (c[i] != d[i]) {
-            cout << "Wrong!\n";
-            return 0;
-        }
+        assert(c[i] == d[i]);
     }
     cout << "Correct!\n";
     return 0;
