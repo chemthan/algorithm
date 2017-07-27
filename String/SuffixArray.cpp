13caa6ad96d9dc549040e98417fb1ac3c6034fc5
Trung Nguyen <chemthan@users.noreply.github.com>
2017-07-27 15:34:51 +0700

    Update

--- String/SuffixArray.cpp
+++ String/SuffixArray.cpp
@@ -16,12 +16,12 @@ struct SuffixArray {
     void build(char* t) {
         strcpy(s, t), n = strlen(s);
         for (int i = 0; i < n; i++) {
-            sa[i] = i, ra[0][i] = 0, ra[1][i] = s[i];
+            sa[i] = i, ra[0][i] = 0, ra[1][i] = s[i] + 1;
         }
         for (int d = 1; radixsort(); d <<= 1) {
             for (int i = 0; i < n; i++) {
                 if (i + d < n) {
-                    ra[1][i] = ra[0][i + d];
+                    ra[1][i] = ra[0][i + d] + 1;
                 }
                 else {
                     ra[1][i] = 0;
@@ -35,7 +35,7 @@ struct SuffixArray {
         static int p[maxn];
         static int tmpsa[maxn];
         static int tmpra[maxn];
-        int mx = max(256, n);
+        int mx = max(256, n) + 1;
         for (int step = 1; step >= 0; step--) {
             fill_n(p, mx, 0);
             for (int i = 0; i < n; i++) {
