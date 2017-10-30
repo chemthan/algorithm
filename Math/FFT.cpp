5a494551e1d7a4dc0833471e284f70fab0e5d351
chemthan <chemthan@gmail.com>
2017-10-31 01:42:14 +0700

    Update

--- Math/FFT.cpp
+++ Math/FFT.cpp
@@ -1,71 +1,87 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long double db;
+#define double long double
+const int MAXF = 1 << 18;
 struct cp {
-    db x, y;
-    cp(db x = 0, db y = 0) : x(x), y(y) {}
-    cp operator + (const cp& rhs) {return cp(x + rhs.x, y + rhs.y);}
-    cp operator - (const cp& rhs) {return cp(x - rhs.x, y - rhs.y);}
-    cp operator * (const cp& rhs) {return cp(x * rhs.x - y * rhs.y, x * rhs.y + y * rhs.x);}
-    cp operator !() {return cp(x, -y);}
-};
-void fft(cp a[], int n, int invert) {
-    static const db PI = acos((db) -1);
-    cp w = cp(cos(2 * PI / n), sin(2 * PI / n));
-    if (invert) w = !w;
-    for (int k = n / 2; k >= 1; k >>= 1, w = w * w) {
-        cp t = cp(1, 0);
-        for (int i = 0; i < k; i++, t = t * w) {
-            for (int j = i; j + k < n; j += k + k) {
-                cp x = a[j];
-                cp y = a[j + k];
-                a[j] = x + y;
-                a[j + k] = (x - y) * t;
+    double x, y;
+    cp(double x = 0, double y = 0) : x(x), y(y) {}
+    cp operator + (const cp& rhs) const {
+        return cp(x + rhs.x, y + rhs.y);
+    }
+    cp operator - (const cp& rhs) const {
+        return cp(x - rhs.x, y - rhs.y);
+    }
+    cp operator * (const cp& rhs) const {
+        return cp(x * rhs.x - y * rhs.y, x * rhs.y + y * rhs.x);
+    }
+    cp operator !() const {
+        return cp(x, -y);
+    }
+} rts[MAXF + 1];
+cp fa[MAXF], fb[MAXF];
+cp fc[MAXF], fd[MAXF];
+
+int bitrev[MAXF];
+void fftinit() {
+    int k = 0; while ((1 << k) < MAXF) k++;
+    bitrev[0] = 0;
+    for (int i = 1; i < MAXF; i++) {
+        bitrev[i] = bitrev[i >> 1] >> 1 | ((i & 1) << k - 1);
+    }
+    double PI = acos((double) -1.0);
+    cp w = cp(cos(2 * PI / MAXF), sin(2 * PI / MAXF));
+    rts[0] = rts[MAXF] = cp(1, 0);
+    for (int i = 1; i + i <= MAXF; i++) {
+        rts[i] = rts[i - 1] * w;
+    }
+    for (int i = MAXF / 2 + 1; i < MAXF; i++) {
+        rts[i] = !rts[MAXF - i];
+    }
+}
+void dft(cp a[], int n, int sign) {
+    int d = 0; while ((1 << d) * n != MAXF) d++;
+    for (int i = 0; i < n; i++) {
+        if (i < (bitrev[i] >> d)) {
+            swap(a[i], a[bitrev[i] >> d]);
+        }
+    }
+    for (int len = 2; len <= n; len <<= 1) {
+        int delta = MAXF / len * sign;
+        for (int i = 0; i < n; i += len) {
+            cp *x = a + i,*y = a + i + (len >> 1), *w = sign > 0 ? rts : rts + MAXF;
+            for (int k = 0; k + k < len; k++) {
+                cp z = *y * *w;
+                *y = *x - z, *x = *x + z;
+                x++, y++, w += delta;
             }
         }
     }
-    int k = __lg(n);
-    for (int msk = 0; msk < n; msk++) {
-        int nmsk = 0;
-        for (int i = 0; i < k; i++) {
-            if ((msk >> i) & 1) {
-                nmsk |= 1 << k - i - 1;
-            }
-        }
-        if (msk < nmsk) {
-            swap(a[msk], a[nmsk]);
-        }
-    }
-    if (invert) {
+    if (sign < 0) {
         for (int i = 0; i < n; i++) {
             a[i].x /= n;
             a[i].y /= n;
         }
     }
 }
-void multiply(int a[], int b[], long long c[], int na, int nb) {
-    static cp fa[1 << 20];
-    static cp fb[1 << 20];
-    int n = na + nb;
-    while (n != (n & -n)) n += n & -n;
-    for (int i = 0; i < n; i++) fa[i] = fb[i] = cp(0, 0);
-    for (int i = 0; i < na; i++) fa[i] = cp(a[i], 0);
-    for (int i = 0; i < nb; i++) fb[i] = cp(b[i], 0);
-    fft(fa, n, 0), fft(fb, n, 0);
+void multiply(int a[], int b[], int na, int nb, long long c[]) {
+    int n = na + nb - 1; while (n != (n & -n)) n += n & -n;
+    for (int i = 0; i < n; i++) fa[i] = fb[i] = cp();
+    for (int i = 0; i < na; i++) fa[i] = cp(a[i]);
+    for (int i = 0; i < nb; i++) fb[i] = cp(b[i]);
+    dft(fa, n, 1), dft(fb, n, 1);
     for (int i = 0; i < n; i++) fa[i] = fa[i] * fb[i];
-    fft(fa, n, 1);
-    for (int i = 0; i < n; i++) c[i] = (long long) (fa[i].x + 0.5);
+    dft(fa, n, -1);
+    for (int i = 0; i < n; i++) c[i] = (long long) floor(fa[i].x + 0.5);
 }
-void multiply(int a[], int b[], int c[], int na, int nb, int mod = (int) 1e9 + 7) {
-    static cp fa[1 << 20], fb[1 << 20];
-    static cp fc[1 << 20], fd[1 << 20];
+void multiply(int a[], int b[], int na, int nb, int c[], int mod = (int) 1e9 + 7) {
     int n = na + nb - 1;
     while (n != (n & -n)) n += n & -n;
-    for (int i = 0; i < n; i++) fa[i] = fb[i] = cp(0, 0);
-    for (int i = 0; i < na; i++) fa[i] = cp(a[i] >> 15, a[i] & (1 << 15) - 1);
-    for (int i = 0; i < nb; i++) fb[i] = cp(b[i] >> 15, b[i] & (1 << 15) - 1);
-    fft(fa, n, 0), fft(fb, n, 0);
+    for (int i = 0; i < n; i++) fa[i] = fb[i] = cp();
+    static const int magic = 15;
+    for (int i = 0; i < na; i++) fa[i] = cp(a[i] >> magic, a[i] & (1 << magic) - 1);
+    for (int i = 0; i < nb; i++) fb[i] = cp(b[i] >> magic, b[i] & (1 << magic) - 1);
+    dft(fa, n, 1), dft(fb, n, 1);
     for (int i = 0; i < n; i++) {
         int j = (n - i) % n;
         cp x = fa[i] + !fa[j];
@@ -75,7 +91,7 @@ void multiply(int a[], int b[], int c[], int na, int nb, int mod = (int) 1e9 + 7
         fc[i] = (x * t + y * z) * cp(0, 0.25);
         fd[i] = x * y * cp(0, 0.25) + z * t * cp(-0.25, 0);
     }
-    fft(fc, n, 1), fft(fd, n, 1);
+    dft(fc, n, -1), dft(fd, n, -1);
     for (int i = 0; i < n; i++) {
         long long u = ((long long) floor(fc[i].x + 0.5)) % mod;
         long long v = ((long long) floor(fd[i].x + 0.5)) % mod;
@@ -83,29 +99,44 @@ void multiply(int a[], int b[], int c[], int na, int nb, int mod = (int) 1e9 + 7
         c[i] = ((u << 15) + v + (w << 30)) % mod;
     }
 }
+vector<int> multiply(vector<int>& a, vector<int>& b, int mod = (int) 1e9 + 7) {
+    static int fa[MAXF], fb[MAXF], fc[MAXF];
+    int na = a.size(), nb = b.size();
+    for (int i = 0; i < na; i++) fa[i] = a[i];
+    for (int i = 0; i < nb; i++) fb[i] = b[i];
+    multiply(fa, fb, na, nb, fc, mod);
+    int k = na + nb - 1;
+    vector<int> res(k);
+    for (int i = 0; i < k; i++) res[i] = fc[i];
+    return res;
+}
 
-const int maxn = 1 << 20;
 const int mod = (int) 1e9 + 7;
-int a[maxn];
-int b[maxn];
-int c[maxn];
-long long d[maxn];
+int n;
+int a[MAXF];
+int b[MAXF];
+int c[MAXF];
+int d[MAXF];
 
 int main() {
+    fftinit();
     srand(time(NULL));
-    int na = 10000, nb = 10000;
-    for (int i = 0; i < na; i++) a[i] = rand() * rand() % 1000000000;
-    for (int i = 0; i < nb; i++) b[i] = rand() * rand() % 1000000000;
-    multiply(a, b, c, na, nb, mod);
-    for (int i = 0; i < na; i++) {
-        for (int j = 0; j < nb; j++) {
-            d[i + j] += (long long) a[i] * b[j];
+    n = 1000;
+    for (int i = 0; i < n; i++) {
+        a[i] = rand();
+        b[i] = rand();
+    }
+    multiply(a, b, n, n, c, mod);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            d[i + j] += (long long) a[i] * b[j] % mod;
             d[i + j] %= mod;
         }
     }
-    for (int i = 0; i < na + nb - 1; i++) {
+    for (int i = 0; i < n + n - 1; i++) {
         assert(c[i] == d[i]);
     }
-    cout << "Correct!\n";
+    cerr << "Correct\n";
+    cerr << "\nTime elapsed: " << 1000 * clock() / CLOCKS_PER_SEC << "ms\n";
     return 0;
 }
