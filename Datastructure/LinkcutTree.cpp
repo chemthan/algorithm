b842b8e69204927ae2bd7337e0fc393b6bb73ed2
chemthan <chemthan@gmail.com>
2018-01-09 10:30:56 +0700

    Update

--- Datastructure/LinkcutTree.cpp
+++ Datastructure/LinkcutTree.cpp
@@ -1,142 +1,168 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*
-* Complexity: amortized O(logn)
-* Problems:
-* 1. http://codeforces.com/problemset/problem/650/E
-* 2. http://www.spoj.com/problems/DYNALCA
-* 3. http://www.spoj.com/problems/DYNACON1
-*/
-struct Node {
-    Node();
-    Node *l, *r, *p;
-    int size, key, cnt;
-    int rev, lz;
+struct node_t {
+    node_t *p, *l, *r;
+    int size, rev;
+    int s, w;
+    node_t() : p(0), l(0), r(0), size(1), rev(0), s(0), w(1) {}
 };
-Node* nil = new Node();
-Node::Node() {
-    l = r = p = nil;
-    size = cnt = 1;
-    key = rev = lz = 0;
+
+int isrt(node_t* x) {
+    return !(x->p) || (x->p->l != x && x->p->r != x);
 }
-void init() {
-    nil->l = nil->r = nil->p = nil;
-    nil->size = nil->cnt = 0;
+
+int left(node_t* x) {
+    return x->p->l == x;
 }
-int isrt(Node* x) {
-    return x->p == nil || (x->p->l != x && x->p->r != x);
+
+void setchild(node_t* x, node_t* p, int l) {
+    (l ? p->l : p->r) = x;
+    if (x) x->p = p;
 }
-void setchild(Node* p, Node* c, int l) {
-    c->p = p; l ? p->l = c : p->r = c;
-}
-void updatelz(Node* x, int val) {
-    if (x == nil) return;
-    x->lz += val;
-    x->cnt += val;
-}
-void pushdown(Node* x) {
-    if (x == nil) return;
-    Node *u = x->l, *v = x->r;
+
+void push(node_t* x) {
+    node_t* u = x->l;
+    node_t* v = x->r;
     if (x->rev) {
-        if (u != nil) {swap(u->l, u->r); u->rev ^= 1;}
-        if (v != nil) {swap(v->l, v->r); v->rev ^= 1;}
+        if (u) swap(u->l, u->r), u->rev ^= 1;
+        if (v) swap(v->l, v->r), v->rev ^= 1;
         x->rev = 0;
     }
-    if (x->lz) {
-        if (u != nil) updatelz(u, x->lz);
-        if (v != nil) updatelz(v, x->lz);
-        x->lz = 0;
-    }
 }
-void pushup(Node* x) {
-    x->size = x->l->size + x->r->size + 1;
+
+int size(node_t* x) {
+    return x ? x->size : 0;
 }
-void rotate(Node* x) {
-    Node* y = x->p;
-    int l = x->p->l == x;
-    if (!isrt(y)) {
-        setchild(y->p, x, y->p->l == y);
-    }
-    else {
-        x->p = y->p;
-    }
-    setchild(y, l ? x->r : x->l, l);
-    setchild(x, y, !l);
-    pushup(y);
+
+int sum(node_t* x) {
+    return x ? x->s : 0;
 }
-void splay(Node* x) {
-    pushdown(x);
+
+void pull(node_t* x) {
+    x->size = size(x->l) + 1 + size(x->r);
+    x->s = sum(x->l) + x->w + sum(x->r);
+}
+
+void rotate(node_t* x) {
+    node_t *p = x->p, *g = p->p;
+    int l = left(x);
+    setchild(l ? x->r : x->l, p, l);
+    if (!isrt(p)) setchild(x, g, left(p));
+    else x->p = g;
+    setchild(p, x, !l);
+    pull(p);
+}
+
+node_t* splay(node_t* x) {
+    push(x);
     while (!isrt(x)) {
-        pushdown(x->p->p); pushdown(x->p); pushdown(x);
-        if (!isrt(x->p)) rotate((x->p->l == x) == (x->p->p->l == x->p) ? x->p : x);
+        node_t *p = x->p, *g = p->p;
+        if (g) push(g);
+        push(p), push(x);
+        if (!isrt(p)) rotate(left(x) != left(p) ? x : p);
         rotate(x);
     }
-    pushup(x);
+    pull(x);
+    return x;
 }
-Node* access(Node* x) {
-    Node* z = nil;
-    for (Node* y = x; y != nil; y = y->p) {
+
+node_t* access(node_t* x) {
+    node_t* z = 0;
+    for (node_t* y = x; y; y = y->p) {
         splay(y);
+        y->w += sum(y->r);
         y->r = z;
-        pushup(z = y);
+        y->w -= sum(y->r);
+        pull(z = y);
     }
     splay(x);
     return z;
 }
-void link(Node* x, Node* y) {
-    access(y); access(x);
-    y->cnt += x->cnt;
-    updatelz(y->l, x->cnt);
-    x->p = y;
-    access(x);
+
+void link(node_t* x, node_t* p) {
+    access(x), access(p);
+    x->p = p;
+    p->w += sum(x);
 }
-void cut(Node* x) {
+
+void cut(node_t* x) {
     access(x);
-    x->l->p = nil;
-    updatelz(x->l, -x->cnt);
-    x->l = nil;
-    pushup(x);
+    x->l->p = 0, x->l = 0;
+    pull(x);
 }
-Node* findroot(Node* x) {
+
+void makeroot(node_t* x) {
     access(x);
-    while (x->l != nil) pushdown(x), x = x->l;
-    splay(x);
-    return x;
+    x->rev ^= 1;
+    swap(x->l, x->r);
 }
-Node* lca(Node* x, Node* y) {
-    if (findroot(x) != findroot(y)) return nil;
+
+node_t* findroot(node_t* x) {
+    access(x);
+    while (x->l) push(x), x = x->l;
+    push(x);
+    return splay(x);
+}
+
+node_t* lca(node_t* x, node_t* y) {
+    if (findroot(x) != findroot(y)) return 0;
     access(x);
     return access(y);
 }
-void makeroot(Node* x) {
-    access(x);
-    swap(x->l, x->r);
-    x->rev ^= 1;
-}
-int connected(Node* x, Node* y) {
+
+int connect(node_t* x, node_t* y) {
     if (x == y) return 1;
-    access(x); access(y);
-    return x->p != nil;
+    access(x), access(y);
+    return x->p != 0;
 }
 
-const int MAXN = 1e5 + 5;
-Node node[MAXN];
+int treequery(node_t* x) {
+    access(x);
+    return x->w;
+}
+
+const int maxn = 1e5 + 5;
+node_t pool[maxn];
+
+void makeroot(int u) {
+    makeroot(pool + u);
+}
+
+void link(int u, int v) {
+    link(pool + u, pool + v);
+}
+
+void cut(int u) {
+    cut(pool + u);
+}
+
+int findroot(int u) {
+    return findroot(pool + u) - pool;
+}
+
+int lca(int u, int v) {
+    node_t* a = lca(pool + u, pool + v);
+    if (!a) return -1;
+    return a - pool;
+}
+
+int connect(int u, int v) {
+    return connect(pool + u, pool + v);
+}
+
+int treequery(int u) {
+    return treequery(pool + u);
+}
 
 int main() {
-    init();
-    int n = 10;
-    for (int i = 1; i <= n; i++) {
-        node[i].key = i;
-    }
-    link(node + 2, node + 1);
-    link(node + 5, node + 1);
-    link(node + 3, node + 2);
-    link(node + 4, node + 2);
-    link(node + 6, node + 5);
-    cout << lca(node + 3, node + 4)->key << "\n";
-    cout << findroot(node + 2)->key << "\n";
-    cut(node + 3);
-    cout << findroot(node + 3)->key << "\n";
+    link(0, 1);
+    link(2, 1);
+    link(3, 2);
+    link(4, 2);
+    link(5, 3);
+    cout << treequery(2) << "\n";
+    makeroot(2);
+    cout << treequery(2) << "\n";
     return 0;
 }
