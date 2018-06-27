ed89a5d4e796017857f10de1279dc53e808b4b92
Trung Nguyen <chemthan@Trungs-MacBook-Air.local>
2018-06-27 23:49:18 +0700

    Update

--- Graph/Bridge & Articulation.cpp
+++ Graph/Bridge & Articulation.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAXN = 100000 + 5;
+const int MAXN = 1e5 + 5;
 int n;
 vector<int> adj[MAXN];
 int num[MAXN];
@@ -10,30 +10,30 @@ int tms;
 int root, nchild;
 
 void dfs(int u, int p = -1) {
-	num[u] = low[u] = ++tms;
-	for (int i = 0; i < (int) adj[u].size(); i++) {
-		int v = adj[u][i];
-		if (!num[v]) {
-			if (u == root) nchild++;
-			dfs(v, u);
-			if (low[v] >= num[u]) {
-				//u is an articulation point
-			}
-			if (low[v] > num[u]) {
-				//u -> v is a bridge
-			}
-			low[u] = min(low[u], low[v]);
-		}
-		else if (v != p) {
-			low[u] = min(low[u], num[v]);
-		}
-	}
+    num[u] = low[u] = ++tms;
+    for (int i = 0; i < (int) adj[u].size(); i++) {
+        int v = adj[u][i];
+        if (!num[v]) {
+            if (u == root) nchild++;
+            dfs(v, u);
+            if (low[v] >= num[u]) {
+                //u is an articulation point
+            }
+            if (low[v] > num[u]) {
+                //u -> v is a bridge
+            }
+            low[u] = min(low[u], low[v]);
+        }
+        else if (v != p) {
+            low[u] = min(low[u], num[v]);
+        }
+    }
 }
 
 int main() {
-	dfs(root = 0);
-	if (nchild > 1) {
-		//root is an articulation point
-	}
-	return 0;
+    dfs(root = 0);
+    if (nchild > 1) {
+        //root is an articulation point
+    }
+    return 0;
 }
