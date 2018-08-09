84cae89cade271cb6a4fe8bdd9f8bd5064212a35
Trung Nguyen <chemthan@Trungs-MacBook-Air.local>
2018-08-10 00:21:48 +0700

    Update

--- Math/PrimitiveRoot.cpp
+++ Math/PrimitiveRoot.cpp
@@ -1,35 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> dvs;
-int fpow(int n, int k, int p) {
-    int r = 1;
-    for (; k; k >>= 1) {
-        if (k & 1) r = (long long) r * n % p;
-        n = (long long) n * n % p;
-    }
-    return r;
-}
-int primitiveroot(int p) {
-    dvs.clear();
-    for (int i = 2; i * i < p; i++) {
-        if ((p - 1) % i) continue;
-        dvs.push_back(i);
-        if (i * i != p - 1) dvs.push_back((p - 1) / i);
-    }
-    for (int i = 2; i < p; i++) {
-        int flag = 1;
-        for (int j = 0; j < dvs.size(); j++) {
-            if (fpow(i, dvs[j], p) == 1) {
-                flag = 0;
-                break;
-            }
+namespace PRT {
+    vector<int> dvs;
+    int fpow(int n, int k, int p) {
+        int r = 1;
+        for (; k; k >>= 1) {
+            if (k & 1) r = (long long) r * n % p;
+            n = (long long) n * n % p;
         }
-        if (flag) return i;
+        return r;
+    }
+    int query(int p) {
+        dvs.clear();
+        for (int i = 2; i * i < p; i++) {
+            if ((p - 1) % i) continue;
+            dvs.push_back(i);
+            if (i * i != p - 1) dvs.push_back((p - 1) / i);
+        }
+        for (int i = 2; i < p; i++) {
+            int flag = 1;
+            for (int j = 0; j < dvs.size(); j++) {
+                if (fpow(i, dvs[j], p) == 1) {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag) return i;
+        }
+        assert(0);
+        return -1;
     }
 }
 
 int main() {
-    cout << primitiveroot(1000000007) << "\n";
+    cout << PRT::query((int) 1e9 + 7) << "\n";
     return 0;
 }
