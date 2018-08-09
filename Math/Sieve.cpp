f38901108a59c09119e96a36aabc0f2a6cfe7dce
Trung Nguyen <chemthan@Trungs-MacBook-Air.local>
2018-08-09 23:48:07 +0700

    Update

---
+++ Math/Sieve.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Sieve {
+    static const int maxn = 123456;
+    static void work(int n) {
+        int k = sqrt(n) + 2;
+        while (k * k > n) k--;
+        static int ispr[maxn];
+        static int prs[maxn];
+        fill_n(ispr + 2, k - 1, 1);
+        int npr = 0;
+        for (int i = 2; i <= k; i++) if (ispr[i]) {
+            for (int j = i * i; j <= k; j += i) {
+                ispr[j] = 0;
+            }
+            prs[npr++] = i;
+        }
+        static int f[maxn];
+        fill_n(f, k + 1, 0);
+        for (int i = k + 1; i <= n; i += k) {
+            int j = min(i + k - 1, n);
+            for (int u = 0; u < npr; u++) {
+                int p = prs[u];
+                for (int q = (i + p - 1) / p * p; q <= j; q += p) {
+                    f[q - i] = i;
+                }
+            }
+            for (int u = i; u <= j; u++) if (f[u - i] < i) {
+                //u is a prime
+            }
+        }
+    }
+};
+
+int main() {
+    int n; cin >> n;
+    Sieve::work(n);
+    return 0;
+}
