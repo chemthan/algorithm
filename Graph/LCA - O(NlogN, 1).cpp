07e6c0f5defe654419867eadf565454ba19cae5e
Trung Nguyen <chemthan@Trungs-MacBook-Air.local>
2018-08-18 10:49:44 +0700

    Update

--- Graph/LCA - O(NlogN, 1).cpp
+++ Graph/LCA - O(NlogN, 1).cpp
@@ -4,7 +4,7 @@ using namespace std;
 namespace LCA {
     const int maxn = 1e5 + 5;
     const int logn = 20;
-    vector<int> event;
+    vector<int> events;
     int sta[maxn];
     int tin[maxn];
     int idx[maxn];
@@ -14,13 +14,13 @@ namespace LCA {
 
     void dfs(int u, int p, vector<int> adj[]) {
         idx[tin[u] = tms++] = u;
-        sta[u] = event.size();
-        event.push_back(tin[u]);
+        sta[u] = events.size();
+        events.push_back(tin[u]);
         for (int i = 0; i < adj[u].size(); i++) {
             int v = adj[u][i];
             if (v != p) {
                 dfs(v, u, adj);
-                event.push_back(tin[u]);
+                events.push_back(tin[u]);
             }
         }
     }
@@ -35,11 +35,11 @@ namespace LCA {
     void build(vector<int> adj[]) {
         events.clear();
         dfs(tms = 0, -1, adj);
-        for (int i = 0; i < event.size(); i++) {
-            f[0][i] = event[i];
+        for (int i = 0; i < events.size(); i++) {
+            f[0][i] = events[i];
         }
         for (int i = 1; i < logn; i++) {
-            for (int j = 0; j + (1 << i - 1) < event.size(); j++) {
+            for (int j = 0; j + (1 << i - 1) < events.size(); j++) {
                 f[i][j] = min(f[i - 1][j], f[i - 1][j + (1 << i - 1)]);
             }
         }
