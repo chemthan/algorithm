06727a4fbf4ac931bba93f932dd737344071dc00
Trung Nguyen <chemthan@Trungs-MacBook-Air.local>
2018-08-21 00:00:12 +0700

    Update

---
+++ Graph/Maxflow.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+* Complexity: O(V^2E), O(min(V^2/3, E^1/2)E) for unit flow
+* Problems:
+* 1. http://codeforces.com/problemset/problem/704/D
+*/
+namespace Maxflow {
+    typedef int flow_t;
+    const int maxv = 1e5 + 5;
+    const int maxe = 1e6 + 5;
+    const flow_t oo = (flow_t) 1e9;
+    int n, s, t, E;
+    int adj[maxe], nxt[maxe], lst[maxv], ptr[maxv], lev[maxv], que[maxv];
+    flow_t flw[maxe], cap[maxe];
+    void init(int nn, int ss, int tt) {
+        n = nn, s = ss, t = tt, E = 0;
+        fill_n(lst, n, -1);
+    }
+    void add(int u, int v, flow_t c1, flow_t c2) {
+        adj[E] = v, flw[E] = 0, cap[E] = c1, nxt[E] = lst[u], lst[u] = E++;
+        adj[E] = u, flw[E] = 0, cap[E] = c2, nxt[E] = lst[v], lst[v] = E++;
+    }
+    int bfs() {
+        fill_n(lev, n, 0), lev[s] = 1;
+        int qsize = 0;
+        que[qsize++] = s;
+        for (int i = 0; i < qsize; i++) {
+            for (int u = que[i], e = lst[u]; ~e; e = nxt[e]) {
+                int v = adj[e];
+                if (flw[e] < cap[e] && !lev[v]) {
+                    lev[v] = lev[u] + 1;
+                    que[qsize++] = v;
+                }
+            }
+        }
+        return lev[t];
+    }
+    flow_t dfs(int u, flow_t bot) {
+        if (u == t) return bot;
+        for (int& e = ptr[u]; ~e; e = nxt[e]) {
+            int v = adj[e];
+            flow_t delta = 0;
+            if (lev[v] == lev[u] + 1 && flw[e] < cap[e] && (delta = dfs(v, min(bot, cap[e] - flw[e]))) > 0) {
+                flw[e] += delta; flw[e ^ 1] -= delta;
+                return delta;
+            }
+        }
+        return 0;
+    }
+    flow_t maxflow(int ss = -1, int tt = -1) {
+        if (~ss) s = ss;
+        if (~tt) t = tt;
+        fill_n(flw, E, 0);
+        flow_t total = 0;
+        while (bfs()) {
+            for (int i = 0; i < n; i++) ptr[i] = lst[i];
+            for (flow_t delta = dfs(s, oo); delta > 0; delta = dfs(s, oo)) total += delta;
+        }
+        return total;
+    }
+    vector<pair<pair<int, int>, flow_t> > gomory_hu() {
+        vector<pair<pair<int, int>, flow_t> > tree;
+        vector<int> p(n);
+        for (int u = 1; u < n; u++) {
+            tree.push_back(make_pair(make_pair(p[u], u), maxflow(u, p[u])));
+            for (int v = u + 1; v < n; ++v) {
+                if (lev[v] && p[v] == p[u]) {
+                    p[v] = u;
+                }
+            }
+        }
+        return tree;
+    }
+}
+
+int main() {
+    Maxflow::init(3, 0, 2);
+    Maxflow::add(0, 1, 3, 0);
+    Maxflow::add(1, 2, 4, 0);
+    assert(Maxflow::maxflow() == 3);
+    cerr << "Correct!\n";
+    cerr << "\nTime elapsed: " << 1000 * clock() / CLOCKS_PER_SEC << "ms\n";
+    return 0;
+}
