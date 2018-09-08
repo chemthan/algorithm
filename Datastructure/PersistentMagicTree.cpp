02c239186dcd21fb35ba6da130354cd091c77c10
chemthan <chemthan@gmail.com>
2018-09-08 22:08:46 +0700

    Update

--- Datastructure/PersistentMagicTree.cpp
+++ Datastructure/PersistentMagicTree.cpp
@@ -8,18 +8,18 @@ using namespace std;
  * Problem:
  * 1. http://vn.spoj.com/problems/VOMARIO/Problems
  */
-typedef int T;
-const T oo = (T) 1e9;
+typedef int num_t;
+const num_t oo = (num_t) 1e9;
 struct func_t {
-    T a, b;
-    func_t(T a = 0, T b = oo) : a(a), b(b) {}
-    T query(T x) {return a * x + b;}
+    num_t a, b;
+    func_t(num_t a = 0, num_t b = oo) : a(a), b(b) {}
+    num_t query(num_t x) {return a * x + b;}
 };
 struct node_t {
     node_t *l, *r;
     func_t f;
     node_t(node_t* l = 0, node_t* r = 0, func_t f = func_t()) : l(l), r(r), f(f) {}
-    T query(T x) {return f.query(x);}
+    num_t query(num_t x) {return f.query(x);}
 };
 node_t* upd(node_t* p, int l, int r, int L, int R, func_t f) {
     if (l > R || r < L) return p;
@@ -27,14 +27,13 @@ node_t* upd(node_t* p, int l, int r, int L, int R, func_t f) {
     node_t* res = p ? new node_t(p->l, p->r, p->f) : new node_t();
     if (l <= L && r >= R) {
         int fl = f.query(L) >= (p ? p->query(L) : oo);
-        int fm1 = f.query(M) >= (p ? p->query(M) : oo);
-        int fm2 = f.query(M + 1) >= (p ? p->query(M + 1) : oo);
         int fr = f.query(R) >= (p ? p->query(R) : oo);
         if (fl && fr) return res;
         if (!fl && !fr) {
             res->f = f;
             return res;
         }
+        int fm1 = f.query(M) >= (p ? p->query(M) : oo);
         if (fl && fm1) {
             res->r = upd(res->r, l, r, M + 1, R, f);
             return res;
@@ -44,6 +43,7 @@ node_t* upd(node_t* p, int l, int r, int L, int R, func_t f) {
             res->f = f;
             return res;
         }
+        int fm2 = f.query(M + 1) >= (p ? p->query(M + 1) : oo);
         if (fm2 && fr) {
             res->l = upd(res->l, l, r, L, M, f);
             return res;
@@ -59,13 +59,13 @@ node_t* upd(node_t* p, int l, int r, int L, int R, func_t f) {
     res->r = upd(res->r, l, r, M + 1, R, f);
     return res;
 }
-node_t* upd(node_t* p, int l, int r, int L, int R, T a, T b) {
+node_t* upd(node_t* p, int l, int r, int L, int R, num_t a, num_t b) {
     return upd(p, l, r, L, R, func_t(a, b));
 }
-T query(node_t* p, int i, int L, int R) {
+num_t query(node_t* p, int i, int L, int R) {
     if (!p) return oo;
     if (i < L || i > R) return oo;
-    T res = p->query(i);
+    num_t res = p->query(i);
     if (L < R) {
         res = min(res, query(p->l, i, L, L + R >> 1));
         res = min(res, query(p->r, i, (L + R >> 1) + 1, R));
