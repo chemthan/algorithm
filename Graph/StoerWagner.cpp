46f0b53518fa56823689470f577fbd57fabb5f44
Trung Nguyen <chemthan@users.noreply.github.com>
2018-11-10 12:01:44 +0700

    Update StoerWagner.cpp

--- Graph/StoerWagner.cpp
+++ Graph/StoerWagner.cpp
@@ -2,75 +2,75 @@
 using namespace std;
 
 /*
-* Find minimum cut in undirected weighted graph
-* Complexity: O(V^3)
-*/
+ * Find minimum cut in undirected weighted graph
+ * Complexity: O(V^3)
+ */
 struct MinCutMatrix {
-  typedef int cap_t;
-  int n;
-  vector<vector<cap_t> > graph;
-  void init(int _n) {
-    n = _n;
-    graph = vector<vector<cap_t>>(n, vector<cap_t>(n, 0));
-  }
-  void addEdge(int a, int b, cap_t w) {
-    if (a == b) return;
-    graph[a][b] += w;
-    graph[b][a] += w;
-  }
-  pair<cap_t, pair<int, int> > stMinCut(vector<int> &active) {
-    vector<cap_t> key(n);
-    vector<int> v(n);
-    int s = -1, t = -1;
-    for (int i = 0; i < active.size(); i++) {
-      cap_t maxv = -1;
-      int cur = -1;
-      for (auto j : active) {
-        if (v[j] == 0 && maxv < key[j]) {
-          maxv = key[j];
-          cur = j;
+    typedef int cap_t;
+    int n;
+    vector<vector<cap_t> > graph;
+    void init(int _n) {
+        n = _n;
+        graph = vector<vector<cap_t>>(n, vector<cap_t>(n, 0));
+    }
+    void addEdge(int a, int b, cap_t w) {
+        if (a == b) return;
+        graph[a][b] += w;
+        graph[b][a] += w;
+    }
+    pair<cap_t, pair<int, int> > stMinCut(vector<int> &active) {
+        vector<cap_t> key(n);
+        vector<int> v(n);
+        int s = -1, t = -1;
+        for (int i = 0; i < active.size(); i++) {
+            cap_t maxv = -1;
+            int cur = -1;
+            for (auto j : active) {
+                if (v[j] == 0 && maxv < key[j]) {
+                    maxv = key[j];
+                    cur = j;
+                }
+            }
+            t = s;
+            s = cur;
+            v[cur] = 1;
+            for (auto j : active) key[j] += graph[cur][j];
         }
-      }
-      t = s;
-      s = cur;
-      v[cur] = 1;
-      for (auto j : active) key[j] += graph[cur][j];
+        return make_pair(key[s], make_pair(s, t));
     }
-    return make_pair(key[s], make_pair(s, t));
-  }
-  vector<int> cut;
-  cap_t solve() {
-    cap_t res = numeric_limits <cap_t>::max();
-    vector<vector<int>> grps;
-    vector<int> active;
-    cut.resize(n);
-    for (int i = 0; i < n; i++) grps.emplace_back(1, i);
-    for (int i = 0; i < n; i++) active.push_back(i);
-    while (active.size() >= 2) {
-      auto stcut = stMinCut(active);
-      if (stcut.first < res) {
-        res = stcut.first;
-        fill(cut.begin(), cut.end(), 0);
-        for (auto v : grps[stcut.second.first]) cut[v] = 1;
-      }
-      int s = stcut.second.first, t = stcut.second.second;
-      if (grps[s].size() < grps[t].size()) swap(s, t);
-      active.erase(find(active.begin(), active.end(), t));
-      grps[s].insert(grps[s].end(), grps[t].begin(), grps[t].end());
-      for (int i = 0; i < n; i++) {
-        graph[i][s] += graph[i][t];
-        graph[i][t] = 0;
-      }
-      for (int i = 0; i < n; i++) {
-        graph[s][i] += graph[t][i];
-        graph[t][i] = 0;
-      }
-      graph[s][s] = 0;
+    vector<int> cut;
+    cap_t solve() {
+        cap_t res = numeric_limits <cap_t>::max();
+        vector<vector<int>> grps;
+        vector<int> active;
+        cut.resize(n);
+        for (int i = 0; i < n; i++) grps.emplace_back(1, i);
+        for (int i = 0; i < n; i++) active.push_back(i);
+        while (active.size() >= 2) {
+            auto stcut = stMinCut(active);
+            if (stcut.first < res) {
+                res = stcut.first;
+                fill(cut.begin(), cut.end(), 0);
+                for (auto v : grps[stcut.second.first]) cut[v] = 1;
+            }
+            int s = stcut.second.first, t = stcut.second.second;
+            if (grps[s].size() < grps[t].size()) swap(s, t);
+            active.erase(find(active.begin(), active.end(), t));
+            grps[s].insert(grps[s].end(), grps[t].begin(), grps[t].end());
+            for (int i = 0; i < n; i++) {
+                graph[i][s] += graph[i][t];
+                graph[i][t] = 0;
+            }
+            for (int i = 0; i < n; i++) {
+                graph[s][i] += graph[t][i];
+                graph[t][i] = 0;
+            }
+            graph[s][s] = 0;
+        }
+        return res;
     }
-    return res;
-  }
 };
 
 int main() {
-	return 0;
+    return 0;
 }
