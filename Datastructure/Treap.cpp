18ebd886e0317922c8d42a9341250da83f45e66f
Trung Nguyen <chemthan@Trungs-MacBook-Pro.local>
2019-02-02 00:11:48 +0700

    Update

--- Datastructure/Treap.cpp
+++ Datastructure/Treap.cpp
@@ -4,23 +4,26 @@ using namespace std;
 /*
  * Complexity: O(logN)
  */
-template<class num_t>
+template<typename num_t>
 struct node_t {
     node_t *l, *r;
     int h, size, rev;
     num_t key, lz;
-    node_t(num_t key) : l(0), r(0), h(rand()), size(1), rev(0), key(key), lz(0) {}
+    node_t(num_t key, node_t* l = 0, node_t* r = 0, int h = rand()) : key(key), l(0), r(0), h(h) {
+        size = (l ? l->size : 0) + 1 + (r ? r->size : 0);
+        rev = lz = 0;
+    }
 };
 
-template<class num_t>
+template<typename num_t>
 int size(node_t<num_t>* x) {
     return x ? x->size : 0;
 }
-template<class num_t>
+template<typename num_t>
 void pull(node_t<num_t>* x) {
     x->size = size(x->l) + 1 + size(x->r);
 }
-template<class num_t>
+template<typename num_t>
 void push(node_t<num_t>* x) {
     node_t<num_t>* u = x->l;
     node_t<num_t>* v = x->r;
@@ -35,7 +38,7 @@ void push(node_t<num_t>* x) {
         x->lz = 0;
     }
 }
-template<class num_t>
+template<typename num_t>
 node_t<num_t>* join(node_t<num_t>* x, node_t<num_t>* y) {
     if (!x) return y;
     if (!y) return x;
@@ -50,7 +53,7 @@ node_t<num_t>* join(node_t<num_t>* x, node_t<num_t>* y) {
     pull(y);
     return y;
 }
-template<class num_t>
+template<typename num_t>
 void split(node_t<num_t>* x, node_t<num_t>*& l, node_t<num_t>*& r, int pos) {
     if (!x) {
         l = r = 0;
@@ -66,12 +69,12 @@ void split(node_t<num_t>* x, node_t<num_t>*& l, node_t<num_t>*& r, int pos) {
         pull(r = x);
     }
 }
-template<class num_t>
+template<typename num_t>
 void split(node_t<num_t>* t, node_t<num_t>*& x, node_t<num_t>*& y, node_t<num_t>*& z, int l, int r) {
     split(t, x, y, l);
     split(y, y, z, r - l + 1);
 }
-template<class num_t>
+template<typename num_t>
 void reverse(node_t<num_t>*& x, int l, int r) {
     node_t<num_t> *y, *z;
     split(x, x, y, z, l, r);
@@ -79,7 +82,7 @@ void reverse(node_t<num_t>*& x, int l, int r) {
     swap(y->l, y->r);
     x = join(x, join(y, z));
 }
-template<class num_t>
+template<typename num_t>
 void upd(node_t<num_t>*& x, int l, int r, num_t val) {
     node_t<num_t> *y, *z;
     split(x, x, y, z, l, r);
@@ -87,13 +90,13 @@ void upd(node_t<num_t>*& x, int l, int r, num_t val) {
     y->key += val;
     x = join(x, join(y, z));
 }
-template<class num_t>
+template<typename num_t>
 int depth(node_t<num_t>* x) {
     if (!x) return 0;
     push(x);
     return 1 + max(depth(x->l), depth(x->r));
 }
-template<class num_t>
+template<typename num_t>
 void trace(node_t<num_t>* x, int isrt = 1) {
     if (!x) return;
     push(x);
@@ -106,9 +109,9 @@ void trace(node_t<num_t>* x, int isrt = 1) {
 }
 
 int main() {
-    srand(time(NULL));
+    srand(time(0));
     node_t<int>* rt = 0;
-    for (int i = 0; i < 100000; i++) {
+    for (int i = 0; i < 1e5; i++) {
         rt = join(rt, new node_t<int>(i));
     }
     node_t<int> *x, *y, *z;
