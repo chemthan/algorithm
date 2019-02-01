e3e41cf4998b924c7d1b483f4c52ae2f9437eb09
Trung Nguyen <chemthan@Trungs-MacBook-Pro.local>
2019-02-02 00:18:36 +0700

    Update

--- Datastructure/SplayTree.cpp
+++ Datastructure/SplayTree.cpp
@@ -1,38 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+ * Complexity: O(logN)
+ */
+
+template<typename num_t>
 struct node_t {
     node_t *p, *l, *r;
-    int key, size;
+    int size;
+    num_t key;
     node_t(int key) : p(0), l(0), r(0), key(key), size(1) {}
 };
 
-int size(node_t* x) {
+template<typename num_t>
+int size(node_t<num_t>* x) {
     return x ? x->size : 0;
 }
-
-int isrt(node_t* x) {
+template<typename num_t>
+int isrt(node_t<num_t>* x) {
     return !(x->p) || (x->p->l != x && x->p->r != x);
 }
-
-int left(node_t* x) {
+template<typename num_t>
+int left(node_t<num_t>* x) {
     return x->p->l == x;
 }
-
-void setchild(node_t* x, node_t* p, int l) {
+template<typename num_t>
+void setchild(node_t<num_t>* x, node_t<num_t>* p, int l) {
     (l ? p->l : p->r) = x;
     if (x) x->p = p;
 }
-
-void push(node_t* x) {
+template<typename num_t>
+void push(node_t<num_t>* x) {
 }
-
-void pull(node_t* x) {
+template<typename num_t>
+void pull(node_t<num_t>* x) {
     x->size = size(x->l) + 1 + size(x->r);
 }
-
-void rotate(node_t* x) {
-    node_t *p = x->p, *g = p->p;
+template<typename num_t>
+void rotate(node_t<num_t>* x) {
+    node_t<num_t> *p = x->p, *g = p->p;
     int l = left(x);
     setchild(l ? x->r : x->l, p, l);
     if (!isrt(p)) setchild(x, g, left(p));
@@ -40,11 +47,11 @@ void rotate(node_t* x) {
     setchild(p, x, !l);
     pull(p);
 }
-
-node_t* splay(node_t* x) {
+template<typename num_t>
+node_t<num_t>* splay(node_t<num_t>* x) {
     push(x);
     while (!isrt(x)) {
-        node_t *p = x->p, *g = p->p;
+        node_t<num_t> *p = x->p, *g = p->p;
         if (g) push(g);
         push(p), push(x);
         if (!isrt(p)) rotate(left(x) != left(p) ? x : p);
@@ -53,18 +60,18 @@ node_t* splay(node_t* x) {
     pull(x);
     return x;
 }
-
-node_t* join(node_t* x, node_t* y) {
+template<typename num_t>
+node_t<num_t>* join(node_t<num_t>* x, node_t<num_t>* y) {
     if (!x) return y;
     while (x->r) push(x), x = x->r;
     push(x);
     setchild(y, x, 0);
     return splay(x);
 }
-
-void split(node_t* t, node_t*& x, node_t*& y, int pos) {
+template<typename num_t>
+void split(node_t<num_t>* t, node_t<num_t>*& x, node_t<num_t>*& y, int pos) {
     if (pos < 0) {
-    	x = 0, y = t;
+        x = 0, y = t;
         return;
     }
     if (pos == size(t) - 1) {
@@ -87,24 +94,30 @@ void split(node_t* t, node_t*& x, node_t*& y, int pos) {
     x->r = y->p = 0;
     pull(x);
 }
-
-void split(node_t* t, node_t*& x, node_t*& y, node_t*& z, int l, int r) {
+template<typename num_t>
+void split(node_t<num_t>* t, node_t<num_t>*& x, node_t<num_t>*& y, node_t<num_t>*& z, int l, int r) {
     split(t, x, y, l - 1), split(y, y, z, r - l);
 }
-
-void trace(node_t* x) {
+template<typename num_t>
+void trace(node_t<num_t>* x, int isrt = 1) {
     if (!x) return;
     push(x);
-    trace(x->l);
-    cout << x->key << " ";
-    trace(x->r);
+    trace(x->l, 0);
+    cerr << x->key << " ";
+    trace(x->r, 0);
+    if (isrt) cerr << "\n";
 }
 
 int main() {
-    node_t* rt = 0;
+    node_t<int>* rt = 0;
     for (int i = 0; i < 10; i++) {
-        rt = join(rt, new node_t(i));
+        rt = join(rt, new node_t<int>(i));
     }
-    trace(rt); cout << "\n";
+    trace(rt);
+    for (int i = 0; i < 1e6; i++) {
+        rt = join(rt, new node_t<int>(i));
+    }
+    cerr << size(rt) << "\n";
+    cerr << "\nTime elapsed: " << 1000 * clock() / CLOCKS_PER_SEC << "ms\n";
     return 0;
 }
