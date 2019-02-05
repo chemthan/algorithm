b3ddd290bf3102a5d6121c1b1febfa5b1cdf9ac6
Trung Nguyen <chemthan@Trungs-MacBook-Air.local>
2019-02-05 19:35:41 +0700

    Update

--- Math/Pollard.cpp
+++ Math/Pollard.cpp
@@ -1,68 +1,106 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template<class T> T mulmod(T a, T b, T p) {
-    a %= p; b %= p;
-    T q = (T) ((long double) a * b / p);
-    T r = a * b - q * p;
-    while (r < 0) r += p;
-    while (r >= p) r -= p;
-    return r;
-    /*
-    T r = 0;
-    int block = 1;
-    T base = 1LL << block;
-    for (; b; b >>= block) {
-        r = (r + a * (b & (base - 1))) % p;
-        a = a * base % p;
-    }
-    return r;
-    */
-}
-template<class T> T powmod(T n, T k, T p) {
-    if (!n) return 0;
-    T r = 1;
-    for (; k; k >>= 1) {
-        if (k & 1) r = mulmod(r, n, p);
-        n = mulmod(n, n, p);
-    }
-    return r;
-}
-template<class T> int rabin(T n) {
-    if (n == 2) return 1;
-    if (n < 2 || !(n & 1)) return 0;
-    const T p[3] = {3, 5, 7};
-    T a, d = n - 1, mx = 3;
-    int i, r, s = 0;
-    while (!(d & 1)) {++s; d >>= 1;}
-    for (i = 0; i < mx; i++) {
-        if (n == p[i]) return 1;
-        if (!(n % p[i])) return 0;
-        a = powmod(p[i], d, n);
-        if (a != 1) {
-            for (r = 0; r < s && a != n - 1; r++) a = mulmod(a, a, n);
-            if (r == s) return 0;
+namespace Pollard {
+    template<class num_t>
+    num_t mulmod(num_t a, num_t b, num_t p) {
+        a %= p; b %= p;
+        num_t q = (num_t) ((long double) a * b / p);
+        num_t r = a * b - q * p;
+        while (r < 0) r += p;
+        while (r >= p) r -= p;
+        return r;
+        /*
+        num_t r = 0;
+        int block = 1;
+        num_t base = 1LL << block;
+        for (; b; b >>= block) {
+            r = (r + a * (b & (base - 1))) % p;
+            a = a * base % p;
         }
+        return r;
+        */
     }
-    return 1;
-}
-//Bruteforce for small n
-template<class T> T pollardrho(T n) {
-    int it = 0, k = 2;
-    T x = 3, y = 3;
-    while (1) {
-        it++;
-        x = (mulmod(x, x, n) + n - 1) % n;
-        T d = __gcd(abs(y - x), n);
-        if (d != 1 && d != n) return d;
-        if (it == k) y = x, k <<= 1;
+    template<class num_t>
+    num_t powmod(num_t n, num_t k, num_t p) {
+        if (!n) return 0;
+        num_t r = 1;
+        for (; k; k >>= 1) {
+            if (k & 1) r = mulmod(r, n, p);
+            n = mulmod(n, n, p);
+        }
+        return r;
+    }
+    template<class num_t>
+    int rabin(num_t n) {
+        if (n == 2) return 1;
+        if (n < 2 || !(n & 1)) return 0;
+        const num_t p[9] = {2, 3, 5, 7, 11, 13, 17, 19, 23};
+        num_t a, d = n - 1, mx = 4;
+        int i, r, s = 0;
+        while (!(d & 1)) {++s; d >>= 1;}
+        for (i = 0; i < mx; i++) {
+            if (n == p[i]) return 1;
+            if (!(n % p[i])) return 0;
+            a = powmod(p[i], d, n);
+            if (a != 1) {
+                for (r = 0; r < s && a != n - 1; r++) a = mulmod(a, a, n);
+                if (r == s) return 0;
+            }
+        }
+        return 1;
+    }
+    template<class num_t>
+    inline num_t f(num_t a, num_t b, num_t n) {
+        num_t res = mulmod(a, a, n) + b;
+        return n <= res ? res - n : res;
+    }
+    template<class num_t>
+    void factorize(num_t n, vector<num_t>& facs) {
+        static int init_seed = 0;
+        if (!init_seed) {
+            init_seed = 1;
+            srand(2311);
+        }
+        if (n == 1) {
+            return;
+        }
+        if (rabin(n)) {
+            facs.push_back(n);
+            return;
+        }
+        if (n == 4) {
+            facs.push_back(2);
+            facs.push_back(2);
+            return;
+        }
+        while (1) {
+            num_t a = rand() & 63, x = 2, y = 2;
+            while (1) {
+                x = f(x, a, n), y = f(f(y, a, n), a, n);
+                if (x == y) break;
+                num_t p = __gcd(n, y <= x ? x - y : y - x);
+                if (p > 1) {
+                    factorize(p, facs), factorize(n / p, facs);
+                    return;
+                }
+            }
+        }
     }
 }
 
 int main() {
-    long long n = 2063512844981574047LL;
-    long long ans = pollardrho(n);
-    assert(n % ans == 0);
-    cout << ans << " " << n / ans << "\n"; //Expected: 1112041493 1855607779
+    srand(time(0));
+    long long n = (long long) rand() * rand();
+    vector<long long> facs;
+    Pollard::factorize(n, facs);
+    long long m = 1;
+    for (int i = 0; i < (int) facs.size(); i++) {
+        cerr << facs[i] << " \n"[i == facs.size() - 1];
+        m *= facs[i];
+        assert(Pollard::rabin(facs[i]));
+    }
+    assert(n == m);
+    cerr << "\nTime elapsed: " << 1000 * clock() / CLOCKS_PER_SEC << "ms\n";
     return 0;
 }
