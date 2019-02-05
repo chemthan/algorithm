7b45ddac069a908a1f8a3cc0c22ac42866e57642
Trung Nguyen <chemthan@Trungs-MacBook-Air.local>
2019-02-05 13:21:46 +0700

    Update

--- Math/Rabin.cpp
+++ Math/Rabin.cpp
@@ -16,61 +16,67 @@ using namespace std;
 //if n < 318,665,857,834,031,151,167,461, it is enough to test a = 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, and 37.
 //if n < 3,317,044,064,679,887,385,961,981, it is enough to test a = 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, and 41.
 
-template<class T> T mulmod(T a, T b, T p) {
-    a %= p; b %= p;
-    T q = (T) ((long double) a * b / p);
-    T r = a * b - q * p;
-    while (r < 0) r += p;
-    while (r >= p) r -= p;
-    return r;
-    /*
-    T r = 0;
-    int block = 1;
-    T base = 1LL << block;
-    for (; b; b >>= block) {
-        r = (r + a * (b & (base - 1))) % p;
-        a = a * base % p;
-    }
-    return r;
-    */
-}
-template<class T> T powmod(T n, T k, T p) {
-    if (!n) return 0;
-    T r = 1;
-    for (; k; k >>= 1) {
-        if (k & 1) r = mulmod(r, n, p);
-        n = mulmod(n, n, p);
-    }
-    return r;
-}
-template<class T> int rabin(T n) {
-    if (n == 2) return 1;
-    if (n < 2 || !(n & 1)) return 0;
-    const T p[3] = {3, 5, 7};
-    T a, d = n - 1, mx = 3;
-    int i, r, s = 0;
-    while (!(d & 1)) {++s; d >>= 1;}
-    for (i = 0; i < mx; i++) {
-        if (n == p[i]) return 1;
-        if (!(n % p[i])) return 0;
-        a = powmod(p[i], d, n);
-        if (a != 1) {
-            for (r = 0; r < s && a != n - 1; r++) a = mulmod(a, a, n);
-            if (r == s) return 0;
+namespace Rabin {
+    template<class num_t>
+    num_t mulmod(num_t a, num_t b, num_t p) {
+        a %= p; b %= p;
+        num_t q = (num_t) ((long double) a * b / p);
+        num_t r = a * b - q * p;
+        while (r < 0) r += p;
+        while (r >= p) r -= p;
+        return r;
+        /*
+        num_t r = 0;
+        int block = 1;
+        num_t base = 1LL << block;
+        for (; b; b >>= block) {
+            r = (r + a * (b & (base - 1))) % p;
+            a = a * base % p;
         }
+        return r;
+        */
     }
-    return 1;
+    template<class num_t>
+    num_t powmod(num_t n, num_t k, num_t p) {
+        if (!n) return 0;
+        num_t r = 1;
+        for (; k; k >>= 1) {
+            if (k & 1) r = mulmod(r, n, p);
+            n = mulmod(n, n, p);
+        }
+        return r;
+    }
+    template<class num_t>
+    int test(num_t n) {
+        if (n == 2) return 1;
+        if (n < 2 || !(n & 1)) return 0;
+        const num_t p[9] = {2, 3, 5, 7, 11, 13, 17, 19, 23};
+        num_t a, d = n - 1, mx = 4;
+        int i, r, s = 0;
+        while (!(d & 1)) {++s; d >>= 1;}
+        for (i = 0; i < mx; i++) {
+            if (n == p[i]) return 1;
+            if (!(n % p[i])) return 0;
+            a = powmod(p[i], d, n);
+            if (a != 1) {
+                for (r = 0; r < s && a != n - 1; r++) a = mulmod(a, a, n);
+                if (r == s) return 0;
+            }
+        }
+        return 1;
+    }
 }
 
 int main() {
-    cout << rabin(1000000007) << "\n";
-    cout << rabin(100000007) << "\n";
-    cout << rabin(10000007) << "\n";
-    cout << rabin(1000007) << "\n";
-    cout << rabin(100007) << "\n";
-    cout << rabin(10007) << "\n";
-    cout << rabin(1007) << "\n";
-    cout << rabin(107) << "\n";
-    cout << rabin(17) << "\n";
+    cerr << Rabin::test(1000000007) << "\n";
+    cerr << Rabin::test(100000007) << "\n";
+    cerr << Rabin::test(10000007) << "\n";
+    cerr << Rabin::test(1000007) << "\n";
+    cerr << Rabin::test(100007) << "\n";
+    cerr << Rabin::test(10007) << "\n";
+    cerr << Rabin::test(1007) << "\n";
+    cerr << Rabin::test(107) << "\n";
+    cerr << Rabin::test(17) << "\n";
+    cerr << "\nTime elapsed: " << 1000 * clock() / CLOCKS_PER_SEC << "ms\n";
     return 0;
 }
