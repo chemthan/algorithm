2114b7cbb9dc8c9650309cced0024c92fe7bebec
Trung Nguyen <chemthan@Trungs-MacBook-Air.local>
2019-03-16 14:07:37 +0700

    Update

--- Math/Sieve.cpp
+++ Math/Sieve.cpp
@@ -2,40 +2,43 @@
 using namespace std;
 
 namespace Sieve {
-    const int maxn = 123456;
+    vector<bool> ispr;
+    vector<int> prs;
     void work(int n) {
         int k = sqrt(n) + 2;
         while (k * k > n) k--;
-        static int ispr[maxn];
-        static int prs[maxn];
-        fill_n(ispr + 2, k - 1, 1);
-        int npr = 0;
-        for (int i = 2; i <= k; i++) if (ispr[i]) {
+        vector<bool> f(k + 1, 1);
+        f[0] = f[1] = 0;
+        ispr.resize(n + 1);
+        ispr.shrink_to_fit();
+        prs.clear();
+        for (int i = 2; i <= k; i++) if (f[i]) {
             for (int j = i * i; j <= k; j += i) {
-                ispr[j] = 0;
+                f[j] = 0;
             }
-            prs[npr++] = i;
+            ispr[i] = 1;
+            prs.push_back(i);
         }
-        static int f[maxn];
-        fill_n(f, k + 1, 0);
+        vector<int> g(k + 1);
+        int c = (int) prs.size();
         for (int i = k + 1; i <= n; i += k) {
             int j = min(i + k - 1, n);
-            for (int u = 0; u < npr; u++) {
+            for (int u = 0; u < c; u++) {
                 int p = prs[u];
                 for (int q = (i + p - 1) / p * p; q <= j; q += p) {
-                    f[q - i] = i;
+                    g[q - i] = i;
                 }
             }
-            for (int u = i; u <= j; u++) if (f[u - i] < i) {
-                //u is a prime
+            for (int u = i; u <= j; u++) if (g[u - i] < i) {
+                ispr[u] = 1;
+                prs.push_back(u);
             }
         }
     }
 }
 
 int main() {
-    Sieve::work((int) 1e9);
-    cerr << "Correct\n";
+    Sieve::work(1e8);
     cerr << "\nTime elapsed: " << 1000 * clock() / CLOCKS_PER_SEC << "ms\n";
     return 0;
 }
