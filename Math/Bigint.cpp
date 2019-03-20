a80c504cff30b5bf50a8e73704b1e38e4d711662
Trung Nguyen <chemthan@Trungs-MacBook-Pro.local>
2019-03-20 15:22:03 +0700

    Update

--- Math/Bigint.cpp
+++ Math/Bigint.cpp
@@ -1,6 +1,135 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define double long double
+namespace FFT {
+    const int maxf = 1 << 18; //Up to one million digits
+    struct cp {
+        double x, y;
+        cp(double x = 0, double y = 0) : x(x), y(y) {}
+        cp operator + (const cp& rhs) const {
+            return cp(x + rhs.x, y + rhs.y);
+        }
+        cp operator - (const cp& rhs) const {
+            return cp(x - rhs.x, y - rhs.y);
+        }
+        cp operator * (const cp& rhs) const {
+            return cp(x * rhs.x - y * rhs.y, x * rhs.y + y * rhs.x);
+        }
+        cp operator !() const {
+            return cp(x, -y);
+        }
+    } rts[maxf + 1];
+    cp fa[maxf], fb[maxf];
+    cp fc[maxf], fd[maxf];
+
+    int bitrev[maxf];
+    void fftinit() {
+        int k = 0; while ((1 << k) < maxf) k++;
+        bitrev[0] = 0;
+        for (int i = 1; i < maxf; i++) {
+            bitrev[i] = bitrev[i >> 1] >> 1 | ((i & 1) << k - 1);
+        }
+        double PI = acos((double) -1.0);
+        rts[0] = rts[maxf] = cp(1, 0);
+        for (int i = 1; i + i <= maxf; i++) {
+            rts[i] = cp(cos(i * 2 * PI / maxf), sin(i * 2 * PI / maxf));
+        }
+        for (int i = maxf / 2 + 1; i < maxf; i++) {
+            rts[i] = !rts[maxf - i];
+        }
+    }
+    void dft(cp a[], int n, int sign) {
+        static int isinit;
+        if (!isinit) {
+            isinit = 1;
+            fftinit();
+        }
+        int d = 0; while ((1 << d) * n != maxf) d++;
+        for (int i = 0; i < n; i++) {
+            if (i < (bitrev[i] >> d)) {
+                swap(a[i], a[bitrev[i] >> d]);
+            }
+        }
+        for (int len = 2; len <= n; len <<= 1) {
+            int delta = maxf / len * sign;
+            for (int i = 0; i < n; i += len) {
+                cp *x = a + i,*y = a + i + (len >> 1), *w = sign > 0 ? rts : rts + maxf;
+                for (int k = 0; k + k < len; k++) {
+                    cp z = *y * *w;
+                    *y = *x - z, *x = *x + z;
+                    x++, y++, w += delta;
+                }
+            }
+        }
+        if (sign < 0) {
+            for (int i = 0; i < n; i++) {
+                a[i].x /= n;
+                a[i].y /= n;
+            }
+        }
+    }
+    void multiply(int a[], int b[], int na, int nb, long long c[]) {
+        int n = na + nb - 1; while (n != (n & -n)) n += n & -n;
+        for (int i = 0; i < n; i++) fa[i] = fb[i] = cp();
+        for (int i = 0; i < na; i++) fa[i] = cp(a[i]);
+        for (int i = 0; i < nb; i++) fb[i] = cp(b[i]);
+        dft(fa, n, 1), dft(fb, n, 1);
+        for (int i = 0; i < n; i++) fa[i] = fa[i] * fb[i];
+        dft(fa, n, -1);
+        for (int i = 0; i < n; i++) c[i] = (long long) floor(fa[i].x + 0.5);
+    }
+    void multiply(int a[], int b[], int na, int nb, int c[], int& nc, int base) {
+        int n = na + nb - 1;
+        while (n != (n & -n)) n += n & -n;
+        for (int i = 0; i < n; i++) fa[i] = fb[i] = cp();
+        static const int magic = 15;
+        for (int i = 0; i < na; i++) fa[i] = cp(a[i] >> magic, a[i] & (1 << magic) - 1);
+        for (int i = 0; i < nb; i++) fb[i] = cp(b[i] >> magic, b[i] & (1 << magic) - 1);
+        dft(fa, n, 1), dft(fb, n, 1);
+        for (int i = 0; i < n; i++) {
+            int j = (n - i) % n;
+            cp x = fa[i] + !fa[j];
+            cp y = fb[i] + !fb[j];
+            cp z = !fa[j] - fa[i];
+            cp t = !fb[j] - fb[i];
+            fc[i] = (x * t + y * z) * cp(0, 0.25);
+            fd[i] = x * y * cp(0, 0.25) + z * t * cp(-0.25, 0);
+        }
+        dft(fc, n, -1), dft(fd, n, -1);
+        nc = 0;
+        long long carry = 0;
+        for (int i = 0; i < n; i++) {
+            long long u = (long long) round(fc[i].x);
+            long long v = (long long) round(fd[i].x);
+            long long w = (long long) round(fd[i].y);
+            long long ncarry = (u / base << 15) + (u % base << 15) / base + v / base + (w / base << 30) + (w % base << 30) / base;
+            assert(0 <= ncarry && ncarry < 2e18);
+            long long t = (u % base << 15) % base + v % base + (w % base << 30) % base + carry;
+            carry = ncarry + t / base;
+            assert(carry < 2e18);
+            c[nc++] = t % base;
+        }
+        while (carry) {
+            c[nc++] = carry % base;
+            carry /= base;
+        }
+    }
+    vector<int> multiply(vector<int> a, vector<int> b, int base) {
+        static int fa[maxf], fb[maxf], fc[maxf + 5];
+        int na = a.size(), nb = b.size();
+        for (int i = 0; i < na; i++) fa[i] = a[i];
+        for (int i = 0; i < nb; i++) fb[i] = b[i];
+        int nc = 0;
+        multiply(fa, fb, na, nb, fc, nc, base);
+        vector<int> res(nc);
+        for (int i = 0; i < nc; i++) res[i] = fc[i];
+        while (1 < res.size() && !res.back()) res.pop_back();
+        return res;
+    }
+}
+#undef double
+
 const int base = 10;
 const int nblock = 9;
 const int blockbase = (int) round(pow(base, nblock));
@@ -264,26 +393,51 @@ struct Bigint {
         return res;
     }
     Bigint operator * (const Bigint& v) const {
-        int r = 6;
-        int t = round(pow(base, r));
-        vector<int> ar = convert_base(this->a, nblock, r);
-        vector<int> br = convert_base(v.a, nblock, r);
-        vector<long long> a(ar.begin(), ar.end());
-        vector<long long> b(br.begin(), br.end());
-        while (a.size() < b.size()) a.push_back(0);
-        while (b.size() < a.size()) b.push_back(0);
-        while (a.size() & (a.size() - 1)) a.push_back(0), b.push_back(0);
-        vector<long long> c = karatsuba(a, b);
-        Bigint res;
-        res.sign = sign * v.sign;
-        for (int i = 0, carry = 0; i < (int) c.size(); i++) {
-            long long cur = c[i] + carry;
-            res.a.push_back((int) (cur % t));
-            carry = (int) (cur / t);
+        if ((max(this->a.size(), v.a.size()) * 9 + 5) / 6 <= 8192) {
+            int r = 6;
+            int t = round(pow(base, r));
+            vector<int> ar = convert_base(this->a, nblock, r);
+            vector<int> br = convert_base(v.a, nblock, r);
+            vector<long long> a(ar.begin(), ar.end());
+            vector<long long> b(br.begin(), br.end());
+            while (a.size() < b.size()) a.push_back(0);
+            while (b.size() < a.size()) b.push_back(0);
+            while (a.size() & (a.size() - 1)) a.push_back(0), b.push_back(0);
+            vector<long long> c = karatsuba(a, b);
+            Bigint res;
+            res.sign = sign * v.sign;
+            long long carry = 0;
+            for (int i = 0; i < (int) c.size(); i++) {
+                long long ncarry = c[i] + carry;
+                res.a.push_back(ncarry % t);
+                carry = ncarry / t;
+            }
+            while (carry) {
+                res.a.push_back(carry % t);
+                carry /= t;
+            }
+            res.a = convert_base(res.a, r, nblock);
+            res.trim();
+            return res;
         }
-        res.a = convert_base(res.a, r, nblock);
-        res.trim();
-        return res;
+        else {
+            vector<int> c = FFT::multiply(this->a, v.a, blockbase);
+            Bigint res;
+            res.sign = sign * v.sign;
+            int carry = 0;
+            for (int i = 0; i < (int) c.size(); i++) {
+                int ncarry = c[i] + carry;
+                res.a.push_back(ncarry % blockbase);
+                carry = ncarry / blockbase;
+            }
+            assert(!carry);
+            while (carry) {
+                res.a.push_back(carry % blockbase);
+                carry /= blockbase;
+            }
+            res.trim();
+            return res;
+        }
     }
     friend Bigint sqrt(const Bigint& a) {
         Bigint x0 = a, x1 = (a + 1) / 2;
